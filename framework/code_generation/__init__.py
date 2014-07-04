b0fa35586e520a46ae50c419da9689357588b867
Nicolas Brunie <nibrunie@gmail.com>
2014-07-04 15:20:21 +0200

    Revert "adding initial file"
    This reverts commit 9d51fa7bfe0a76e90e6503dd5169020a5bd182c0.

--- framework/code_generation/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from c_code_generator import *
-from code_object import *
-from generator_utility import *
-from generic_processor import *
--- framework/testsuite/integration_test.sh
+++
@@ -1,8 +0,0 @@
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rn sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_sqrt_fp64.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rz sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_sqrt_fp64.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=ru sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_sqrt_fp64.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rd sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_sqrt_fp64.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rn sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_div.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rz sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_div.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=ru sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_div.sh && \
-TEST_START=0 EXTRA_GEN_OPTION= SEED=$SEED RND_MODE=rd sh /work1/hardware/users/nbrunie/tools/metalibm/framework/testsuite/test_ml_div.sh 
