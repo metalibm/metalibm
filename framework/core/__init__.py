0b2a3d7963b08dcfcdcbce656fe5ae8de119df8e
Nicolas Brunie <nicolas.brunie@kalray.eu>
2014-09-24 10:09:39 +0200

    deleting files

--- framework/core/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from attributes import *
-from ml_formats import *
-from ml_operations import *
-from polynomials import *
--- framework/utility/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from common import *
-from log_report import *
