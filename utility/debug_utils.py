dc5e3573370add04d78c4099c59f929ff59aab44
Nicolas Brunie <nicolas.brunie@kalray.eu>
2014-11-10 15:25:00 +0100

    adding missing module debug_utils

---
+++ utility/debug_utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from metalibm_core.core.attributes import ML_Debug
+
+# debug utilities
+# display single precision and double precision numbers
+debugf        = ML_Debug(display_format = "%f")
+
+debuglf       = ML_Debug(display_format = "%lf")
+
+# display hexadecimal format for integer
+debugx        = ML_Debug(display_format = "%x")
+
+# display 64-bit hexadecimal format for integer
+debuglx       = ML_Debug(display_format = "%\"PRIx64\"", )
+
+# display long/int integer
+debugd        = ML_Debug(display_format = "%d", pre_process = lambda v: "(int) %s" % v)
+
+# display long long/ long int integer
+debugld        = ML_Debug(display_format = "%ld")
+
+# display hexadecimal encoding of double precision fp number
+debug_lftolx  = ML_Debug(display_format = "%\"PRIx64\"", pre_process = lambda v: "double_to_64b_encoding(%s)" % v)
+debug_lftolx_k1  = ML_Debug(display_format = "%\"PRIx64\" ev=%x", pre_process = lambda v: "double_to_64b_encoding(%s), __k1_fpu_get_exceptions()" % v)
+
+# display hexadecimal encoding of double double fp number
+debug_ddtolx    = ML_Debug(display_format = "%\"PRIx64\" %\"PRIx64\"", pre_process = lambda v: "double_to_64b_encoding(%s.hi), double_to_64b_encoding(%s.lo)" % (v, v))
+
+# display floating-point value of double double fp number
+debug_dd      = ML_Debug(display_format = "{.hi=%lf, .lo=%lf}", pre_process = lambda v: "%s.hi, %s.lo" % (v, v))
