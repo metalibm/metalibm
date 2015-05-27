94558d92f6c36bf35743c7d5a0634f5205685de1
Nicolas Brunie <nbrunie@kalray.eu>
2015-05-27 14:33:14 +0200

    dev new version of ML_Function (as a functor) + updating ml_log, ml_log10, ml_log1p and ml_log2 to use the new ML_Function + update non_regression valid

--- valid/non_regression.py
+++ valid/non_regression.py
@@ -2,25 +2,42 @@
 
 import commands
 
-def test_function(script_name, options = []):
-  status, out = commands.getstatusoutput("python metalibm_functions/%s.py %s" % (script_name, " ".join(options)))
-  if status != 0:
-    print out
-    print "%s \033[31;1m FAILED \033[0;m" % script_name
-  else:
-    print "%s SUCCESS" % script_name
-  return (status == 0)
+
+import metalibm_functions.ml_log10
+import metalibm_functions.ml_log1p
+import metalibm_functions.ml_log2
+import metalibm_functions.ml_log
 
 function_list = [
-  "ml_log",
-  "ml_log2",
-  "ml_log1p",
-  "ml_log10",
+  metalibm_functions.ml_log2.ML_Log2,
+  metalibm_functions.ml_log10.ML_Log10,
+  metalibm_functions.ml_log1p.ML_Log1p,
+  metalibm_functions.ml_log.ML_Log,
 ]
 
+def test_function(function_ctor, options = []):
+  function_name = function_ctor.get_name()
+  try: 
+    fct = function_ctor()
+  except:
+    return False
+  return True
+
+
+
 success = True
+result_map = {}
 for function in function_list:
-  success &= test_function(function)
+  test_result = test_function(function)
+  result_map[function] = test_result 
+  success &= test_result
+
+for function in result_map:
+  function_name = function.get_name()
+  if not result_map[function]:
+    print "%s \033[31;1m FAILED \033[0;m" % function_name
+  else:
+    print "%s SUCCESS" % function_name
 
 if success:
   print "OVERALL SUCCESS"
