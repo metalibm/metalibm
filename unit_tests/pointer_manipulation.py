74158f11b4f7e28b0e76ae0c9f6e48d6bc540c16
Nicolas Brunie <nbrunie@kalray.eu>
2015-06-09 10:34:59 +0200

    starting to add emulate implementation for ml_log2 + adding pointer manipulation unit test

---
+++ unit_tests/pointer_manipulation.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from pythonsollya import *
+
+from metalibm_core.core.ml_function import ML_Function, ML_FunctionBasis
+
+from metalibm_core.core.attributes import ML_Debug
+from metalibm_core.core.ml_operations import *
+
+from metalibm_core.core.ml_formats import *
+from metalibm_core.core.ml_complex_formats import * 
+
+from metalibm_core.code_generation.c_code_generator import CCodeGenerator
+from metalibm_core.code_generation.generic_processor import GenericProcessor
+from metalibm_core.code_generation.mpfr_backend import MPFRProcessor
+from metalibm_core.code_generation.code_object import CodeObject
+from metalibm_core.code_generation.code_element import CodeFunction
+from metalibm_core.code_generation.code_constant import C_Code 
+from metalibm_core.core.ml_optimization_engine import OptimizationEngine
+from metalibm_core.core.polynomials import *
+from metalibm_core.core.ml_table import ML_Table
+
+from metalibm_core.code_generation.gappa_code_generator import GappaCodeGenerator
+
+from metalibm_core.utility.gappa_utils import execute_gappa_script_extract
+from metalibm_core.utility.ml_template import ML_ArgTemplate
+
+from metalibm_core.utility.arg_utils import test_flag_option, extract_option_value  
+
+from metalibm_core.utility.debug_utils import * 
+
+class ML_UT_PointerManipulation(ML_Function("ml_ut_pointer_manipulation")):
+  def __init__(self, 
+                 precision = ML_Binary32, 
+                 abs_accuracy = S2**-24, 
+                 libm_compliant = True, 
+                 debug_flag = False, 
+                 fuse_fma = True, 
+                 fast_path_extract = True,
+                 target = MPFRProcessor(), 
+                 output_file = "ut_function_format.c", 
+                 function_name = "ut_function_format"):
+    io_precisions = [precision] * 2
+
+    # initializing base class
+    ML_FunctionBasis.__init__(self, 
+      base_name = "log10",
+      function_name = function_name,
+      output_file = output_file,
+
+      io_precisions = io_precisions,
+      abs_accuracy = None,
+      libm_compliant = libm_compliant,
+
+      processor = target,
+      fuse_fma = fuse_fma,
+      fast_path_extract = fast_path_extract,
+
+      debug_flag = debug_flag
+    )
+
+    self.precision = precision
+
+
+  def generate_scheme(self):
+    #func_implementation = CodeFunction(self.function_name, output_format = self.precision)
+    vx = self.implementation.add_input_variable("x", ML_Binary32)
+    px = self.implementation.add_input_variable("px", ML_Binary32_p)
+
+    result = vx * vx
+    #result.set_precision(ML_Binary32)
+    #vx_assign = ReferenceAssign(px, result)
+    px_assign = ReferenceAssign(Dereference(px, precision = ML_Binary32), result)
+    scheme = Statement(px_assign)
+
+    return scheme
+
+if __name__ == "__main__":
+  # auto-test
+  arg_template = ML_ArgTemplate(default_function_name = "new_ut_function_format", default_output_file = "new_ut_function_format.c" )
+  arg_template.sys_arg_extraction()
+
+
+  ml_ut_pointer_manipulation = ML_UT_PointerManipulation(arg_template.precision, 
+                                libm_compliant            = arg_template.libm_compliant, 
+                                debug_flag                = arg_template.debug_flag, 
+                                target                    = arg_template.target, 
+                                fuse_fma                  = arg_template.fuse_fma, 
+                                fast_path_extract         = arg_template.fast_path,
+                                function_name             = arg_template.function_name,
+                                output_file               = arg_template.output_file)
+
+  ml_ut_pointer_manipulation.gen_implementation()
