82447cf6ca4710ad61000144a1de70aa1fb81c46
Julien Le Maire <jlemaire@kalray.eu>
2015-10-08 17:18:15 +0200

    non working version of log_fixed64, but start exploring parameters of argument reduction

--- ml_log_fixed64.py
+++ ml_log_fixed64.py
@@ -22,6 +22,8 @@ from metalibm_core.utility.gappa_utils import execute_gappa_script_extract
 from metalibm_core.utility.ml_template import ML_ArgTemplate
 from metalibm_core.utility.debug_utils import * 
 
+from numpy import ndindex as ndrange # multidimentional range to iterate over
+
 class ML_Log(ML_Function("ml_log")):
   def __init__(self, 
                precision = ML_Binary64, 
@@ -68,7 +70,6 @@ class ML_Log(ML_Function("ml_log")):
     return mpfr_call
 
 
-
   # explore the parameters of the argument reduction
   # get the fastest code possible with some memory constraint :
   # for all possible parameters of the arg reg:
@@ -79,65 +80,159 @@ class ML_Log(ML_Function("ml_log")):
   # - get the smallest degree that achieve 2^-~128 absolute precision
   #   (TODO: get exact limit with worst cases. should be around 2^-114)
   # of all the parameters that achived thoses degrees, choose the one that have the smallest table size
-  def generate_argument_reduction(self, vx, memory_bytes_limit):
-    # TODO: make thoses parameters vary
-    size1 = 7
-    prec1 = 9
-    size2 = 14
-    prec2 = 15
+  """ return the size of the tables used by the argument reduction,
+      and the interval of the output variable (and some other infos about the argument reduction= """ 
+  def evaluate_argument_reduction(self, x, size1, prec1, size2, prec2):
+    one = Constant(1, precision = ML_Exact, tag = "one")
     
-    #vx = Variable("gappa_x", precision = ML_Custom_FixedPoint_Format(0, 52, False), interval = Interval(1,2-2**-52))
-    vx.set_interval(Interval(1, 2-S2**-52))
+    arg_reduc = {
+      # parameters
+      'size1': size1, 'prec1': prec1, 'size2': size2, 'prec2': prec2,
+      # utilities
+      'prec_inv1': ML_Custom_FixedPoint_Format(1, prec1, False),
+      'prec_inv2': ML_Custom_FixedPoint_Format(1, prec2, False),
+      # results
+      'size_table1': None, 'size_table2': None, 'size_tables_byte': None,
+      'midinterval': None, 'outinterval': None,
+    }
+
+    # do the argument reduction
+    x1 =    Conversion(x, tag = "x1",
+                       precision = ML_Custom_FixedPoint_Format(0,size1,False),
+                       rounding_mode = ML_RoundTowardMinusInfty)
+    s = Multiplication(Subtraction(x1, one, precision = ML_Exact),
+                       Constant(S2**size1, precision = ML_Exact),
+                       precision = ML_Exact,
+                       tag = "indexTableX")
+    inv_x1 =  Division(one, x1, tag = "ix1",
+                       precision = ML_Exact)
+    inv_x = Conversion(inv_x1,  tag = "ix",
+                       precision = arg_reduc['prec_inv1'],
+                       rounding_mode = ML_RoundTowardPlusInfty)
+    y = Multiplication(x, inv_x, tag = "y",
+                       precision = ML_Exact)
+    dy =   Subtraction(y, one,  tag = "dy", 
+                       precision = ML_Exact)
+    y1 =    Conversion(y, tag = "y",
+                       precision = ML_Custom_FixedPoint_Format(0,size2,False),
+                       rounding_mode = ML_RoundTowardMinusInfty)
+    t = Multiplication(Subtraction(y1, one, precision = ML_Exact),
+                       Constant(S2**size2, precision = ML_Exact),
+                       precision = ML_Exact,
+                       tag = "indexTableY")
+    inv_y1 =  Division(one, y1, tag = "iy1",
+                       precision = ML_Exact)
+    inv_y = Conversion(inv_y1, tag = "iy",
+                       precision = arg_reduc['prec_inv2'],
+                       rounding_mode = ML_RoundTowardPlusInfty)
+    z = Multiplication(y, inv_y, tag = "z",
+                       precision = ML_Exact)
+    dz =   Subtraction(z, one, tag = "dz",
+                       precision = ML_Exact)
+
+    # add the necessary goals and hints
+    x_gappa = Variable("x_gappa", interval = Interval(1, 2-S2**-52), precision = ML_Binary64)
+    swap_map = {x: x_gappa}
+
+    # goal: dz (result of the argument reduction)
+    gappa_code = self.gappa_engine.get_interval_code_no_copy(dz.copy(swap_map), bound_list = [x_gappa])
+    self.gappa_engine.add_goal(gappa_code, dy.copy(swap_map))
+    self.gappa_engine.add_goal(gappa_code, s.copy(swap_map)) # range of index of table 1
+    self.gappa_engine.add_goal(gappa_code, t.copy(swap_map)) # range of index of table 2
+    # hints. are the ones with isAppox=True really necessary ?
+    self.gappa_engine.add_hint(gappa_code, x.copy(swap_map), x1.copy(swap_map), isApprox = True)
+    self.gappa_engine.add_hint(gappa_code, y.copy(swap_map), y1.copy(swap_map), isApprox = True)
+    self.gappa_engine.add_hint(gappa_code, inv_x1.copy(swap_map), inv_x.copy(swap_map), isApprox = True)
+    self.gappa_engine.add_hint(gappa_code, inv_y1.copy(swap_map), inv_y.copy(swap_map), isApprox = True)
+    self.gappa_engine.add_hint(gappa_code,
+                               Multiplication(x1, inv_x1, precision = ML_Exact).copy(swap_map), one,
+                               Comparison(swap_map[inv_x1], Constant(0, precision = ML_Exact),
+                                          specifier = Comparison.NotEqual, precision = ML_Bool))
+    self.gappa_engine.add_hint(gappa_code,
+                               Multiplication(y1, inv_y1, precision = ML_Exact).copy(swap_map), one,
+                               Comparison(swap_map[inv_y1], Constant(0, precision = ML_Exact),
+                                          specifier = Comparison.NotEqual, precision = ML_Bool))
+    # execute and parse the result
+    result = execute_gappa_script_extract(gappa_code.get(self.gappa_engine))
+    arg_reduc['size_table1'] = 1+sup(result['indexTableX']).getConstantAsInt()
+    arg_reduc['size_table2'] = 1+sup(result['indexTableY']).getConstantAsInt()
+    arg_reduc['midinterval'] = result['dy']
+    arg_reduc['outinterval'] = result['goal']
+    arg_reduc['size_tables_byte'] = arg_reduc['size_table1'] * (16 + arg_reduc['prec_inv1'].get_c_bit_size()/8) + arg_reduc['size_table2'] * (16 + arg_reduc['prec_inv2'].get_c_bit_size()/8)
+    return arg_reduc
+
+  def generate_argument_reduction(self, x, memory_limit):
+    possible_arg_reducs = []
+    best_arg_reduc = None
+    # iterate through all possible values, and keep only the valid argument reduction
+    # uses some usefull properties to reduce the space of parameters:
+    #   size1 <= log2(memory_limit) - 4
+    #   prec1 < -13 + 
+    #   midinterval <= 2**-size1 + 2**(1-prec1)
+    #memory_used >= 16 * size_table_1 == 16 * 2**size1. so size1 <= log2(memory_limit) - 4
+    #  - prec2 don't influence the table sizes, and increasing it improve the argument reduction
+    #    so it is the max such that sup(outinterval) <= 2**(64-52-prec1-prec2)
+    #    but outinterval can't be mush smaller than 2**-(size2 + 1)
+    #    so: 2**-(size2+2) <= 2**(64-52-prec1-prec2)
+
+    max_size1 = floor(log(S2 * memory_limit)/log(S2) - 5).getConstantAsInt()
+    print "===== size1 <=", max_size1, " =====", memory_limit, (log(S2 * memory_limit)/log(S2) - 5)
+    for size1 in range(2,max_size1):
+      max_prec1 = ceil(log((1 + S2**12) * (1 - S2**-(size1+1)))/log(S2)).getConstantAsInt() - 1
+      print "===== prec1 <=", max_prec1, '=====', (1 - S2**-(size1+1)).getConstantAsDouble()
+      for prec1 in range(size1,10):
+        tmpres = self.evaluate_argument_reduction(x, size1, prec1, prec1, prec1)
+        
+        doBreak_size2 = False
+        for size2 in range(prec1,15):
+          if doBreak_size2:
+            break
+          for prec2 in range(size2,15):
+            arg_reduc = self.evaluate_argument_reduction(x, size1, prec1, size2, prec2)
+
+            # check the memory used
+            if arg_reduc['size_tables_byte'] > memory_limit:
+              doBreak_size2 = True
+              break
+
+            # check if the result of the first arg_red goes into a uint64_t
+            midinterval = arg_reduc['midinterval']
+            max_on_u64 = S2**-(52 - 64 + arg_reduc['prec_inv1'].get_frac_size())
+            #print "Implicit bits 1:", (-1 - floor(log(sup(midinterval))/log(2)).getConstantAsInt())
+            #print "Res Interval 1: {0} < {1} == 2^64*2^-(52+{2}+{3})".format(
+            #  sup(midinterval), max_on_u64, arg_reduc['prec_inv1'].get_frac_size(), arg_reduc['prec_inv2'].get_frac_size())
+            #assert(0 <= inf(midinterval) and sup(midinterval) < max_on_u64)
+            if not(0 <= inf(midinterval) and sup(midinterval) <= max_on_u64):
+              continue 
+
+            # check if the result of the two arg_red goes into a uint64_t
+            outinterval = arg_reduc['outinterval']
+            max_on_u64 = S2**-(52 - 64 + arg_reduc['prec_inv1'].get_frac_size() + arg_reduc['prec_inv2'].get_frac_size())
+            #print "Implicit bits 2:", (-1 - floor(log(sup(outinterval))/log(2)).getConstantAsInt())
+            #print "Res Interval 2: {0} < {1} == 2^64*2^-(52+{2}+{3})".format(
+            #  sup(outinterval), max_on_u64, arg_reduc['prec_inv1'].get_frac_size(), arg_reduc['prec_inv2'].get_frac_size())
+            #assert(0 <= inf(outinterval) and sup(outinterval) < max_on_u64)
+            if not(0 <= inf(outinterval) and sup(outinterval) < max_on_u64):
+              continue
+            
+            print "Potentially valid argument reduction: ", size1, prec1, size2, prec2
+            #print "  memory: ", size_in_bytes
+            #print "  midinterval: ", sup(midinterval).getConstantAsDouble()
+            #print "  outinterval: ", sup(outinterval).getConstantAsDouble()
+            possible_arg_reducs.append(arg_reduc)
+            if (best_arg_reduc is None) or sup(outinterval).getConstantAsDouble() > sup(best_arg_reduc['outinterval']).getConstantAsDouble():
+              best_arg_reduc = arg_reduc
+    print "\n\nBest arg reduc: \n", arg_reduc, "\n"
     
-    #vx  = self.implementation.add_input_variable("gappa_x", ML_Custom_FixedPoint_Format(0,52,False))
-    vx1 = Conversion(vx, precision = ML_Custom_FixedPoint_Format(0,size1,False), tag = "x1")
-    vinv_x1 = Division(Constant(1, precision = ML_Exact), vx1, precision = ML_Exact, tag = "inv_x1")
-    vinv_x = Conversion(vinv_x1, precision = ML_Custom_FixedPoint_Format(0, prec1), tage = "inv_x")
-    vy = Multiplication(vx, vinv_x, precision = ML_Exact) - Constant(1, precision = ML_Exact)#ML_Custom_FixedPoint_Format(0, 52+prec1))
+    return best_arg_reduc
     
-    #self.precison = ML_Binary64
-    #opt_expr = self.optimise_scheme(vy)
-    
-    annotation = Multiplication(vx1, vinv_x1, precision = ML_Exact)
-    #print annotation.get_str(depth = True, display_precision = True)
-
-    vx_me = Variable("me", interval = vx.get_interval(), precision = ML_Binary64)
-    swap_map = {vx: vx_me }
-    vy_goal = vy.copy(swap_map)
-
-    annotation_hint = annotation.copy(swap_map)
-
-    
-    gappa_code = self.gappa_engine.get_interval_code_no_copy(vy_goal, bound_list = [vx_me])
-    print "annotation: ", 
-    print annotation.get_str(depth = None, display_precision = True)
-    print "annotation_hint: ", 
-    print annotation_hint.get_str(depth = None, display_precision = True)
-    print "Gappa engine: ", dir(self.gappa_engine)
-    #self.gappa_engine.add_hint(gappa_code, 
-    self.gappa_engine.add_hint(gappa_code, annotation_hint, Constant(1, precision = ML_Exact),
-                               Comparison(swap_map[vinv_x1], Constant(0, precision = ML_Exact), specifier = Comparison.NotEqual, precision = ML_Bool))
-
-    #print gappa_code.get_str()
-    eval_error = execute_gappa_script_extract(gappa_code.get(self.gappa_engine))
-    print "vy_goal: ", vy_goal.get_str(depth = None, display_precision = True, memoization_map = {})
-    print "eval error: ", eval_error
-    sys.exit(1)
-    return {
-      'size1': 7, 'prec1': 9, 'size2': 14, 'prec2': 15,
-      'prec_inv1': ML_Custom_FixedPoint_Format(1, 9, False),
-      'prec_inv2': ML_Custom_FixedPoint_Format(1, 15, False),
-      'tableSize1': 128,      'tableSize2': 192,
-      'outinterval': Interval(0, 197375.0*2**-31)}
-
-
   def round_nearest_fixed(x, prec):
     return nearestint(x * 2**(-prec)) * 2**prec
 
 
   def generate_scheme(self):
     input_var = self.implementation.add_input_variable("input_var", self.precision) 
-
+    memory_limit = 4000
     sollya_precision = self.precision.sollya_object
 
     # local overloading of RaiseReturn operation
@@ -162,16 +257,16 @@ class ML_Log(ML_Function("ml_log")):
     # compute the parameters for the argument reduction, given the specified constraints
     ve = ExponentExtraction(input_var, tag = "x_exponent", debug = debugd)
     vx = MantissaExtraction(input_var, tag = "x_mantissa", debug = debug_lftolx, precision = self.precision)
-    arg_reduc = self.generate_argument_reduction(vx, 2560)
+    arg_reduc = self.generate_argument_reduction(vx, memory_limit)
 
     # create the tables for the first argument reduction
-    inv_table_1 = ML_Table(dimensions = [arg_reduc['tableSize1'], 1],
+    inv_table_1 = ML_Table(dimensions = [arg_reduc['size_table1'], 1],
                            storage_precision = arg_reduc['prec_inv1'],
                            tag = self.uniquify_name("inv_table_1"))
-    log_table_1 = ML_Table(dimensions = [arg_reduc['tableSize1'], 1],
+    log_table_1 = ML_Table(dimensions = [arg_reduc['size_table1'], 1],
                            storage_precision = ML_Custom_FixedPoint_Format(11, 128-11, False),
                            tag = self.uniquify_name("inv_table_1"))
-    for i in xrange(0, arg_reduc['tableSize1']-1):
+    for i in xrange(0, arg_reduc['size_table1']-1):
       x1 = 1 + i/2**arg_reduc['size1']
       inv_x1 = Division(Constant(1, precision = ML_Exact), x1,
                         precision = arg_reduc['prec_inv1'],
@@ -181,13 +276,13 @@ class ML_Log(ML_Function("ml_log")):
       log_table_1[i][0] = log_x1
 
     # create the tables for the second argument reduction
-    inv_table_2 = ML_Table(dimensions = [arg_reduc['tableSize2'], 1],
+    inv_table_2 = ML_Table(dimensions = [arg_reduc['size_table2'], 1],
                            storage_precision = arg_reduc['prec_inv2'],
                            tag = self.uniquify_name("inv_table_1"))
-    log_table_2 = ML_Table(dimensions = [arg_reduc['tableSize2'], 1],
+    log_table_2 = ML_Table(dimensions = [arg_reduc['size_table2'], 1],
                            storage_precision = ML_Custom_FixedPoint_Format(11, 128-11, False),
                            tag = self.uniquify_name("inv_table_2"))
-    for i in xrange(0, arg_reduc['tableSize2']-1):
+    for i in xrange(0, arg_reduc['size_table2']-1):
       x1 = 1 + i/2**arg_reduc['size2']
       inv_x1 = Division(Constant(1, precision = ML_Exact), x1,
                         precision = arg_reduc['prec_inv2'],
