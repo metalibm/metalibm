dc9e388b8592d3585a39aeb94bd0afc966a01fc6
Nicolas Brunie <nbrunie@kalray.eu>
2015-12-14 18:38:36 +0100

    extending ComplexOperator documentation

--- code_generation/complex_generator.py
+++ code_generation/complex_generator.py
@@ -19,18 +19,28 @@ from ..core.ml_operations import *
 from .generator_utility import *
 
 
-
+## complex generator for symbol-like operators, 
+#  the generate expression modifies the optree before calling the 
+#  code_generator.generate_expr
+#
 class ComplexOperator(ML_CG_Operator):
-  """ complex generator for symbol-like operators, 
-      the generate expression modifies the optree before calling the 
-      code_generator.generate_expr
-      """
 
+  ## constructor
   def __init__(self, optree_modifier, backup_operator = None, **kwords):
     ML_CG_Operator.__init__(self, **kwords)
+    ## function used to modify optree before code generation
     self.optree_modifier = optree_modifier
+    ## generator used when optree returned by modifier is None
     self.backup_operator = backup_operator
 
+  ## generate expression for operator
+  # @param self current operator
+  # @param code_generator CodeGenerator object used has helper for code generation services
+  # @param code_object    CobeObject receiving generated code
+  # @param optree         Operation object being generated
+  # @param arg_tuple      tuple of optree's arguments
+  # @param generate_pre_process  lambda function (None by default) used in preprocessing
+  # @param kwords         generic keywords dictionnary (see ML_CG_Operator() class for list of supported arguments)
   def generate_expr(self, code_generator, code_object, optree, arg_tuple, generate_pre_process = None, **kwords):
     new_optree = self.optree_modifier(optree)
     if new_optree is None and self.backup_operator != None:
