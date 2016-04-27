6adaec3815b06238ca5e4482f8fd692930884e18
Nicolas Brunie <nbrunie@kalray.eu>
2016-04-27 21:54:39 +0200

    Changing version number to 0.1c

--- metalibm_core/utility/version_info.py
+++ metalibm_core/utility/version_info.py
@@ -4,6 +4,6 @@ import commands, inspect, os
 
 script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 git_sha = commands.getoutput("""pushd %s > /dev/null &&  git log -n 1 --pretty=format:"%%H" && popd > /dev/null """ % script_dir)
-version_num = "0.1"
+version_num = "0.1c"
 version_description = "alpha"
 notes = """ metalibm core """
