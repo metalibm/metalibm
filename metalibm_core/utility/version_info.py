8be8ecfe7f1a4e26884b7cd52bc796f24220ae0e
Nicolas Brunie <nibrunie@gmail.com>
2016-11-28 22:53:01 +0100

    Updating version_info with basic hdl support

--- metalibm_core/utility/version_info.py
+++ metalibm_core/utility/version_info.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# Version history:
+#  0.1d: adding vhdl support (backend, code generation, ml_entity and code_entity)
+#
+#
+
 import commands, inspect, os
 
 script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-git_sha = commands.getoutput("""pushd %s > /dev/null &&  git log -n 1 --pretty=format:"%%H" && popd > /dev/null """ % script_dir)
-version_num = "0.1c"
+cmd = """cd %s &&  git log -n 1 --pretty=format:"%%H" """ % script_dir
+print "git cmd: ", cmd
+git_sha = commands.getoutput(cmd)
+version_num = "0.1d"
 version_description = "alpha"
 notes = """ metalibm core """
