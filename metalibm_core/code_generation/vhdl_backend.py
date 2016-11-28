215741790368c872d9f20f726af22c06babd0f5e
Nicolas Brunie <nibrunie@gmail.com>
2016-11-28 22:50:51 +0100

    Extending vhdl backend support (Subtraction, Comparison, LogicalAnd, Event)

--- metalibm_core/code_generation/vhdl_backend.py
+++ metalibm_core/code_generation/vhdl_backend.py
@@ -16,9 +16,10 @@ from ..utility.log_report import *
 from .generator_utility import *
 from .code_element import *
 from ..core.ml_formats import *
-from ..core.ml_hdl_format import ML_StdLogicVectorFormat
+from ..core.ml_hdl_format import *
 from ..core.ml_table import ML_ApproxTable
 from ..core.ml_operations import *
+from ..core.ml_hdl_operations import *
 from metalibm_core.core.target import TargetRegister
 
 
@@ -28,6 +29,15 @@ def exclude_std_logic(optree):
   return not isinstance(optree.get_precision(), ML_StdLogicVectorFormat)
 def include_std_logic(optree):
   return isinstance(optree.get_precision(), ML_StdLogicVectorFormat)
+
+vhdl_comp_symbol = {
+  Comparison.Equal: "=", 
+  Comparison.NotEqual: "/=",
+  Comparison.Less: "<",
+  Comparison.LessOrEqual: "<=",
+  Comparison.GreaterOrEqual: ">=",
+  Comparison.Greater: ">"
+}
 
 # class Match custom std logic vector format
 MCSTDLOGICV = TCM(ML_StdLogicVectorFormat)
@@ -44,6 +54,44 @@ vhdl_code_generation_table = {
       
     }
   },
+  Subtraction: {
+    None: {
+      exclude_std_logic: 
+          build_simplified_operator_generation_nomap([ML_Int8, ML_UInt8, ML_Int16, ML_UInt16, ML_Int32, ML_UInt32, ML_Int64, ML_UInt64, ML_Int128,ML_UInt128], 2, SymbolOperator("-", arity = 2), cond = (lambda _: True)),
+      include_std_logic:
+      {
+        type_custom_match(MCSTDLOGICV, MCSTDLOGICV, MCSTDLOGICV):  SymbolOperator("-", arity = 2),
+      }
+      
+    }
+  },
+  LogicalAnd: {
+    None: {
+      lambda _: True : {
+        type_strict_match(ML_Bool, ML_Bool, ML_Bool): SymbolOperator("and", arity = 2, force_folding = True),
+      },
+    }
+  },
+  Event: {
+    None: {
+      lambda _: True : {
+        type_strict_match(ML_Bool, ML_StdLogic): SymbolOperator("\'event", lspace = "", inverse = True, arity = 1, force_folding = True), 
+      },
+    },
+  },
+  Comparison: 
+      dict (
+        (specifier,
+          { 
+              lambda _: True: {
+                  type_custom_match(FSM(ML_Bool), TCM(ML_StdLogicVectorFormat), TCM(ML_StdLogicVectorFormat)): 
+                    SymbolOperator(vhdl_comp_symbol[specifier], arity = 2, force_folding = True),
+                  type_strict_match(ML_Bool, ML_StdLogic, ML_StdLogic):
+                    SymbolOperator(vhdl_comp_symbol[specifier], arity = 2, force_folding = True),
+              },
+              #build_simplified_operator_generation([ML_Int32, ML_Int64, ML_UInt64, ML_UInt32, ML_Binary32, ML_Binary64], 2, SymbolOperator(">=", arity = 2), result_precision = ML_Int32),
+          }) for specifier in [Comparison.Equal, Comparison.NotEqual, Comparison.Greater, Comparison.GreaterOrEqual, Comparison.Less, Comparison.LessOrEqual]
+  ),
 }
  
 
