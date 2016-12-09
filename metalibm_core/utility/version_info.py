eaec0ec326eeee02f12a408f95fe8bea031bad91
Hugues de Lassus <hugues.de-lassus@univ-perp.fr>
2016-12-09 11:52:27 +0100

    FIX Do not use pushd/popd and quote paths.
    Fixes an error when the called shell does not provide pushd/popd
    commands.
    
    Quoting paths may prevent bugs when whitespaces are present.

--- metalibm_core/utility/version_info.py
+++ metalibm_core/utility/version_info.py
@@ -2,8 +2,11 @@
 
 import commands, inspect, os
 
+cwd = os.getcwd()
 script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-git_sha = commands.getoutput("""pushd %s > /dev/null &&  git log -n 1 --pretty=format:"%%H" && popd > /dev/null """ % script_dir)
+git_sha = commands.getoutput("""cd '%s' > /dev/null && \
+        git log -n 1 --pretty=format:"%%H" && \
+        cd '%s' > /dev/null """ % (script_dir, cwd))
 version_num = "0.1c"
 version_description = "alpha"
 notes = """ metalibm core """
