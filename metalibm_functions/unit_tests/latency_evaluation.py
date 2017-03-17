ecb528af88fd44f987c226bc1ae13b83ae551acd
Nicolas Brunie <nibrunie@gmail.com>
2017-03-17 22:21:09 +0100

    adding unit-test to validate latency evaluation feature

---
+++ metalibm_functions/unit_tests/latency_evaluation.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from sollya import S2
+
+from metalibm_core.core.ml_function import ML_Function, ML_FunctionBasis
+
+from metalibm_core.core.attributes import ML_Debug
+from metalibm_core.core.ml_operations import *
+
+from metalibm_core.core.ml_formats import *
+from metalibm_core.core.ml_complex_formats import * 
+
+from metalibm_core.code_generation.c_code_generator import CCodeGenerator
+from metalibm_core.code_generation.generic_processor import GenericProcessor
+from metalibm_core.code_generation.mpfr_backend import MPFRProcessor
+from metalibm_core.code_generation.code_constant import C_Code 
+from metalibm_core.core.ml_optimization_engine import OptimizationEngine
+
+
+from metalibm_core.utility.ml_template import *
+
+
+class LatencyEvaluator:
+  def __init__(self, target, language = C_Code):
+    self.target = target
+    ## dictionnary storing pair (optree, latency) of already 
+    #  evaluated nodes
+    self.latency_map = {}
+    self.language = language
+
+  ## Evaluate the critical path latency of 
+  #  the evaluation of @p optree
+  def evaluate(self, optree):
+    if optree in self.latency_map:
+      return self.latency_map[optree]
+    else:
+      if isinstance(optree, ML_ArithmeticOperation):
+        optree_impl = self.target.get_implementation(optree, language = self.language)
+        latency = optree_impl.get_speed_measure() + max([self.evaluate(inp) for inp in optree.get_inputs()])
+        self.latency_map[optree] = latency
+        return latency
+      if isinstance(optree, ML_LeafNode):
+        return 1.0
+      else:
+        latency = max([self.evaluate(inp) for inp in optree.get_inputs()])
+        self.latency_map[optree] = latency
+        return latency
+
+class ML_UT_LatencyEvaluation(ML_Function("ml_ut_latency_evaluation")):
+  def __init__(self, 
+                 arg_template,
+                 precision = ML_Binary32, 
+                 abs_accuracy = S2**-24, 
+                 libm_compliant = True, 
+                 debug_flag = False, 
+                 fuse_fma = True, 
+                 fast_path_extract = True,
+                 target = MPFRProcessor(), 
+                 output_file = "ut_latency_evaluation.c", 
+                 function_name = "ut_latency_evaluation"):
+    # precision argument extraction
+    precision = ArgDefault.select_value([arg_template.precision, precision])
+    io_precisions = [precision] * 2
+
+    # initializing base class
+    ML_FunctionBasis.__init__(self, 
+      base_name = "ut_latency_evaluation",
+      function_name = function_name,
+      output_file = output_file,
+
+      io_precisions = io_precisions,
+      abs_accuracy = None,
+      libm_compliant = libm_compliant,
+
+      processor = target,
+      fuse_fma = fuse_fma,
+      fast_path_extract = fast_path_extract,
+
+      debug_flag = debug_flag,
+      arg_template = arg_template,
+    )
+
+    self.precision = precision
+
+
+  def generate_scheme(self):
+    vx = self.implementation.add_input_variable("x", self.precision)
+
+    operations = Multiplication(
+      Addition(vx, vx, precision = self.precision),
+      Constant(3, precision = self.precision),
+      precision = self.precision
+    )
+
+    latency_pass  = LatencyEvaluator(self.processor)
+    latency_value = latency_pass.evaluate(operations)
+    print "latency evaluation result: {}".format(latency_value)
+    
+    scheme = Statement(
+              Return(operations, precision = self.precision)
+            )
+    return scheme
+
+def run_test(args):
+  ml_ut_latency_evaluation = ML_UT_LatencyEvaluation(args)
+  ml_ut_latency_evaluation.gen_implementation(display_after_gen = True, display_after_opt = True)
+  return True
+
+if __name__ == "__main__":
+  # auto-test
+  arg_template = ML_NewArgTemplate("new_ut_latency_eval", default_output_file = "new_ut_latency_eval.c" )
+  args = arg_template.arg_extraction()
+
+  if run_test(args):
+    exit(0)
+  else:
+    exit(1)
