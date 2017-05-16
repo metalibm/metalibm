c666bcc614a5c9f57f7444da61edbc444ec4edf7
Nicolas Brunie <nbrunie@kalray.eu>
2017-05-16 11:08:24 +0200

    Adding Test.IsSubnormal implementation in VectorBackend
    Details:
    *   + Adding new ml_vtestf<n>_is_subnormal routines to vector_lib
    *   + Associating routine to Test.IsSubnormal in VectorBackend

--- metalibm_core/targets/common/vector_backend.py
+++ metalibm_core/targets/common/vector_backend.py
@@ -950,7 +950,15 @@ vector_c_code_generation_table = {
         type_strict_match(v4bool, v4float32): ML_VectorLib_Function("ml_vtestf4_is_nan_or_inf", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v4int32),
         type_strict_match(v8bool, v8float32): ML_VectorLib_Function("ml_vtestf8_is_nan_or_inf", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v8int32),
       },
-    }
+    },
+    Test.IsSubnormal: {
+      lambda _: True: {
+        type_strict_match(v2bool, v2float32): ML_VectorLib_Function("ml_vtestf2_is_subnormal", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v2int32),
+        type_strict_match(v3bool, v3float32): ML_VectorLib_Function("ml_vtestf3_is_subnormal", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v3int32),
+        type_strict_match(v4bool, v4float32): ML_VectorLib_Function("ml_vtestf4_is_subnormal", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v4int32),
+        type_strict_match(v8bool, v8float32): ML_VectorLib_Function("ml_vtestf8_is_subnormal", arg_map = {0: FO_ResultRef(0), 1: FO_Arg(0)}, arity = 1, output_precision = v8int32),
+      },
+    },
   },
 }
 
