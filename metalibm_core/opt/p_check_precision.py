8e9223ed68371614640779e4c233ffbe6add2f60
Nicolas Brunie <nibrunie@gmail.com>
2017-06-01 22:57:33 +0200

    Improving Pass_CheckPrecision
    Details:
    *   + Pass_CheckPrecision is now able to check None
    *     precision for Statement-like operations

--- metalibm_core/opt/p_check_precision.py
+++ metalibm_core/opt/p_check_precision.py
@@ -2,10 +2,21 @@
 # optimization pass to promote a scalar/vector DAG into vector registers
 
 from metalibm_core.core.ml_formats import *
-from metalibm_core.core.ml_operations import ML_LeafNode
+from metalibm_core.core.ml_operations import ML_LeafNode, Statement, ConditionBlock, ReferenceAssign
+from metalibm_core.core.ml_hdl_operations import Process, Loop, ComponentInstance
 from metalibm_core.core.passes import OptreeOptimization, Pass
 
 
+## Check if @p optree has a valid precision
+def check_precision_validity(optree):
+  none_class_list = [
+    Statement, ConditionBlock, Process,
+    ReferenceAssign, Loop, ComponentInstance
+  ]
+  if reduce(lambda x, y: x or y, [isinstance(optree, none_class) for none_class in none_class_list], False):
+    return optree.get_precision() is None
+  else:
+    return not optree.get_precision() is None
 
 ## Generic vector promotion pass
 class Pass_CheckPrecision(OptreeOptimization):
@@ -20,10 +31,10 @@ class Pass_CheckPrecision(OptreeOptimization):
     if optree in self.memoization_map: 
       return self.memoization_map[optree]
     else:
-      precision_validity = not optree.get_precision() is None
+      precision_validity = check_precision_validity(optree)
       self.memoization_map[optree] = precision_validity
       if not precision_validity:
-        Log.Report(Log.Info, 
+        Log.report(Log.Info, 
           "the following node has no defined precision: {}".format(
             optree.get_str(
               depth = 2, 
