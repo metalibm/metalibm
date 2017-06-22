636ba3817592a602796101c967d87de6926256b6
Nicolas Brunie <nibrunie@gmail.com>
2017-06-22 17:14:34 +0200

    Implementing unit-bench to automatically extract micro-architecture

---
+++ metalibm_functions/unit_bench.py
@@ -0,0 +1,315 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+import sollya
+
+from sollya import (
+        S2, Interval, ceil, floor, round, inf, sup, pi, log, exp, cos, sin,
+        guessdegree, dirtyinfnorm
+)
+
+from metalibm_core.core.ml_function import ML_Function, ML_FunctionBasis, DefaultArgTemplate
+
+from metalibm_core.core.attributes import ML_Debug
+from metalibm_core.core.ml_operations import *
+from metalibm_core.core.ml_formats import *
+from metalibm_core.core.polynomials import *
+from metalibm_core.core.ml_table import ML_NewTable
+from metalibm_core.code_generation.generator_utility import FunctionOperator, FO_Result, FO_Arg
+
+from metalibm_core.utility.ml_template import ML_NewArgTemplate, ArgDefault, precision_parser
+from metalibm_core.utility.log_report  import Log
+from metalibm_core.utility.debug_utils import *
+from metalibm_core.utility.num_utils   import ulp
+
+class OpUnitBench:
+  ## @param op_class Operation class (to build new operation instance)
+  #  @param op_arity Number of arguments expected by the operation
+  #  @param init_interval Range of the first arguments to the operation
+  #  @param renorm_function optree -> optree function renormalizing the 
+  #         result of a series of Operation to avoid overflow / underflow
+  #  @param output_precision format of the operation output
+  #  @param input_precisions list of input operand formats
+  def __init__(self, op_class, bench_name, op_arity = 2, init_interval = Interval(-0.5, 0.5), renorm_function = lambda x: x, output_precision = ML_Binary32, input_precisions = [ML_Binary32, ML_Binary32]):
+    self.op_class = op_class
+    self.op_arity = op_arity 
+    self.init_interval = init_interval
+    self.renorm_function  = renorm_function
+    self.output_precision = output_precision
+    self.input_precisions = input_precisions
+    self.bench_name       = bench_name
+
+  ## generate bench  
+  def generate_bench(self, test_num = 1000, unroll_factor = 10): 
+    initial_inputs = [
+      Constant(
+        random.uniform(
+          inf(self.init_interval),
+          sup(self.init_interval)
+        ), precision = precision
+      ) for i in xrange(self.op_arity)
+    ]
+
+    var_inputs = [Variable("var_%d" % i, precision = precision, var_type = Variable.Local) for i, precision in enumerate(self.input_precisions)]
+
+    printf_timing_op = FunctionOperator(
+      "printf", 
+      arg_map = {
+        0: "\"%s[%s] %%lld elts computed in %%lld cycles => %%.3f CPE \\n\"" % 
+          (
+            self.bench_name, 
+            self.output_precision.get_display_format()
+          ), 
+        1: FO_Arg(0), 
+        2: FO_Arg(1), 
+        3: FO_Arg(2), 
+        4: FO_Arg(3)
+      }, void_function = True
+    ) 
+    printf_timing_function = FunctionObject(
+      "printf", 
+      [self.output_precision, ML_Int64, ML_Int64, ML_Binary64], 
+      ML_Void, printf_timing_op
+    )
+    timer = Variable("timer", precision = ML_Int64, var_type = Variable.Local)
+
+    void_function_op = FunctionOperator("(void)", arity = 1, void_function = True)
+    void_function    = FunctionObject(
+      "(void)", 
+      [self.output_precision], 
+      ML_Void, void_function_op
+    )
+
+    # initialization of operation inputs
+    init_assign = Statement()
+    for var_input, init_value in zip(var_inputs, initial_inputs):
+      init_assign.push(ReferenceAssign(var_input, init_value))
+
+    # test loop
+    vi = Variable("i", precision = ML_Int64, var_type = Variable.Local)
+    test_num_cst = Constant(
+      test_num / unroll_factor, 
+      precision = ML_Int64, 
+      tag = "test_num"
+    )
+
+    # Goal build a chain of dependant operation to measure
+    # elementary operation latency
+    local_inputs  = tuple(var_inputs)
+    local_result = operation(*local_inputs, precision = self.output_precision)
+    input_list = var_inputs
+    for i in xrange(unroll_factor - 1):
+      local_inputs = tuple([local_result] + var_inputs[1:])
+      local_result = operation(*local_inputs, precision = self.output_precision)
+    # renormalisation
+    local_result = self.renorm_function(local_result)
+
+    # variable assignation to build dependency chain
+    var_assign = Statement()
+    var_assign.push(ReferenceAssign(var_inputs[0], local_result))
+    final_value = var_inputs[0]
+
+    # loop increment value
+    loop_increment = 1
+
+    test_loop = Loop(
+      ReferenceAssign(vi, Constant(0, precision = ML_Int32)),
+      vi < test_num_cst,
+      Statement(
+        var_assign,
+        ReferenceAssign(vi, vi + loop_increment)
+      ),
+    )
+
+    # bench scheme
+    test_scheme = Statement(
+      ReferenceAssign(timer, self.processor.get_current_timestamp()),
+      init_assign,
+      test_loop,
+
+      ReferenceAssign(timer, 
+        Subtraction(
+          self.processor.get_current_timestamp(),
+          timer,
+          precision = ML_Int64
+        )
+      ),
+      # prevent intermediary variable simplification
+      void_function(final_value),
+      printf_timing_function(
+        final_value,
+        Constant(test_num, precision = ML_Int64),
+        timer,
+        Division(
+          Conversion(timer, precision = ML_Binary64),
+          Constant(test_num, precision = ML_Binary64),
+          precision = ML_Binary64
+        )
+      ),
+      Return(Constant(0, precision = ML_Int32))
+    )
+
+    return test_scheme
+
+operator_bench_map = {
+  Addition:       OpUnitBench(Addition, 2, Interval(-0.1, 0.1)),
+  Subtraction:    OpUnitBench(Addition, 2, Interval(-0.1, 0.1)),
+  Multiplication: OpUnitBench(Addition, 2, Interval(0.9999, 1.0001)),
+  Division:       OpUnitBench(Addition, 2, Interval(0.9999, 1.0001)),
+}
+
+
+
+class ML_UnitBench(ML_Function("ml_external_bench")):
+  """ Implementation of external bench function wrapper """
+  def __init__(self, 
+             arg_template = DefaultArgTemplate, 
+               output_file = "bench.c", 
+               function_name = "main", 
+               ):
+    arity = 0
+
+    # initializing base class
+    ML_FunctionBasis.__init__(self, 
+      base_name = "bench",
+      function_name = function_name,
+      output_file = output_file,
+      arity = arity,
+      arg_template = arg_template
+    )
+    # initializing I/O precision
+
+    self.headers = arg_template.headers
+    self.libraries = arg_template.libraries
+    self.bench_function_name = arg_template.bench_function_name
+    # number of basic iteration
+    self.test_num = arg_template.test_num
+    self.unroll_factor = arg_template.unroll_factor
+
+    self.emulate = arg_template.emulate
+
+  @staticmethod
+  def get_default_args(**kw):
+    default_values = {
+      "precision": ML_Int32,
+    }
+    default_values.update(kw)
+    return DefaultArgTemplate(**default_values)
+
+  def generate_scheme(self): 
+    operation     = Multiplication
+    function_name = "Addition"
+    precision     = ML_Binary32
+    arity         = 2
+    unroll_factor = self.unroll_factor
+    test_num      = self.test_num
+
+    initial_inputs = [Constant(random.uniform(0.99999, 1.0001), precision = precision) 
+                       for i in xrange(arity)]
+
+    var_inputs = [Variable("var_%d" % i, precision = precision, var_type = Variable.Local) for i in xrange(arity)]
+
+    printf_timing_op = FunctionOperator("printf", arg_map = {0: "\"%s[%s] %%lld elts computed in %%lld cycles => %%.3f CPE \\n\"" % (function_name, precision.get_display_format()), 1: FO_Arg(0), 2: FO_Arg(1), 3: FO_Arg(2), 4: FO_Arg(3)}, void_function = True) 
+    printf_timing_function = FunctionObject("printf", [precision, ML_Int64, ML_Int64, ML_Binary64], ML_Void, printf_timing_op)
+    timer = Variable("timer", precision = ML_Int64, var_type = Variable.Local)
+
+    void_function_op = FunctionOperator("(void)", arity = 1, void_function = True)
+    void_function    = FunctionObject("(void)", [precision], ML_Void, void_function_op)
+
+    # test loop
+    vi = Variable("i", precision = ML_Int64, var_type = Variable.Local)
+    test_num_cst = Constant(test_num / unroll_factor, precision = ML_Int64, tag = "test_num")
+
+    # Goal build a chain of dependant operation to measure
+    # elementary operation latency
+    local_inputs  = tuple(var_inputs)
+    local_result = operation(*local_inputs, precision = precision)
+    input_list = var_inputs
+    for i in xrange(unroll_factor - 1):
+      local_inputs = tuple([local_result] + var_inputs[1:])
+      local_result = operation(*local_inputs, precision = precision)
+    # renormalisation
+    #renorm_factor = ExponentInsertion(
+    #  - ExponentExtraction(local_result, precision = ML_Int32),
+    #  precision = precision
+    #)
+    #local_result = Multiplication(renorm_factor, local_result, precision = precision)
+
+    var_assign = Statement()
+    init_assign = Statement()
+    var_assign.push(ReferenceAssign(var_inputs[0], local_result))
+    final_value = var_inputs[0]
+
+    for var_input, init_value in zip(var_inputs, initial_inputs):
+      init_assign.push(ReferenceAssign(var_input, init_value))
+
+    loop_increment = 1
+
+    test_loop = Loop(
+      ReferenceAssign(vi, Constant(0, precision = ML_Int32)),
+      vi < test_num_cst,
+      Statement(
+        var_assign,
+        ReferenceAssign(vi, vi + loop_increment)
+      ),
+    )
+
+    # bench scheme
+    test_scheme = Statement(
+      ReferenceAssign(timer, self.processor.get_current_timestamp()),
+      init_assign,
+      test_loop,
+
+
+
+      ReferenceAssign(timer, 
+        Subtraction(
+          self.processor.get_current_timestamp(),
+          timer,
+          precision = ML_Int64
+        )
+      ),
+      # prevent intermediary variable simplification
+      void_function(final_value),
+      printf_timing_function(
+        final_value,
+        Constant(test_num, precision = ML_Int64),
+        timer,
+        Division(
+          Conversion(timer, precision = ML_Binary64),
+          Constant(test_num, precision = ML_Binary64),
+          precision = ML_Binary64
+        )
+      ),
+      Return(Constant(0, precision = ML_Int32))
+    )
+
+    return test_scheme
+
+
+  def numeric_emulate(self, *args):
+    return self.emulate(*args)
+
+if __name__ == "__main__":
+  # auto-test
+  arg_template = ML_NewArgTemplate(default_function_name = "main", default_output_file = "bench.c" , default_arg = ML_UnitBench.get_default_args())
+  def precision_list_parser(s):
+    return [precision_parser(p) for p in s.split(",")]
+
+  # argument extraction 
+  arg_template.get_parser().add_argument("--function", dest = "bench_function_name", default = "expf", action = "store", type = str, help = "name of the function to be benched")
+  #arg_template.get_parser().add_argument("--input-formats", dest = "input_formats", default = [ML_Binary32], action = "store", type = precision_list_parser, help = "comma separated list of input precision")
+  arg_template.get_parser().add_argument("--test-num", dest = "test_num", default = 10000, action = "store", type = int, help = "number of basic iteration")
+  arg_template.get_parser().add_argument("--unroll-factor", dest = "unroll_factor", default = 10, action = "store", type = int, help = "number of basic iteration")
+  arg_template.get_parser().add_argument("--headers", dest = "headers", default = [], action = "store", type = lambda s: s.split(","), help = "comma separated list of required headers")
+  arg_template.get_parser().add_argument("--libraries", dest = "libraries", default = [], action = "store", type = lambda s: s.split(","), help = "comma separated list of required libraries")
+  def local_eval(s):
+    return eval(s)
+  arg_template.get_parser().add_argument("--emulate", dest = "emulate", default = lambda x: x, action = "store", type = local_eval, help = "function numeric emulation")
+
+
+  args = arg_template.arg_extraction()
+
+  ml_sincos = ML_UnitBench(args)
+  ml_sincos.gen_implementation()
