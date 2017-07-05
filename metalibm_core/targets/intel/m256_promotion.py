3136f204c4cba1418de956d6df2889e2b805e77d
Hugues de Lassus <hugues.de-lassus@univ-perp.fr>
2017-07-05 14:27:43 +0200

    Remove __m128 promotions in M256_Promotion pass

--- metalibm_core/targets/intel/m256_promotion.py
+++ metalibm_core/targets/intel/m256_promotion.py
@@ -13,17 +13,11 @@ from metalibm_core.opt.p_vector_promotion import Pass_Vector_Promotion
 class Pass_M256_Promotion(Pass_Vector_Promotion):
   pass_tag = "m256_promotion"
   ## Translation table between standard formats
-  #  and __m128-based register formats
+  #  and __m256-based register formats
   trans_table = {
-    ML_Binary32: ML_SSE_m128_v1float32,
-    ML_Binary64: ML_SSE_m128_v1float64,
-    v2float64:   ML_SSE_m128_v2float64,
     v4float64:   ML_AVX_m256_v4float64,
-    v4float32:   ML_SSE_m128_v4float32,
     v8float32:   ML_AVX_m256_v8float32,
-    v4int32:     ML_SSE_m128_v4int32,
     v8int32:     ML_AVX_m256_v8int32,
-    ML_Int32:    ML_SSE_m128_v1int32,
   }
 
   def get_translation_table(self):
