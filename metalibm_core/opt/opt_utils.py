f2141850301871973e2e73d63ccb3a629c5e5d75
Nicolas Brunie <nibrunie@gmail.com>
2017-08-08 17:40:20 +0200

    Adding new opt.opt_utils module with evaluate_range function
    Details:
    *    + evaluate_range function computes recursively the interval of an
    *      Operation node
    *    + adding unit-test range_eval to validate evaluate_range
    *      functionnality

---
+++ metalibm_core/opt/opt_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyrights: Nicolas Brunie (2017)
+# email: nibrunie@gmail.com
+#
+# Created:       August, 8th 2017
+# Last-modified: August, 8th 2017
+
+from metalibm_core.core.ml_operations import (
+    ML_LeafNode, Comparison
+)
+
+
+def evaluate_comparison_range(optree):
+    return None
+
+def is_comparison(optree):
+    return isinstance(optree, Comparison)
+
+## Assuming @p optree has no pre-defined range, recursively compute a range
+#  from the node inputs
+def evaluate_range(optree):
+    """ evaluate the range of an Operation node
+
+        Args:
+            optree (ML_Operation): input Node
+
+        Return:
+            sollya Interval: evaluated range of optree or None if no range
+                             could be determined 
+    """
+    init_interval =  optree.get_interval()
+    if not init_interval is None:
+        return init_interval
+    else:
+        if isinstance(optree, ML_LeafNode):
+            return optree.get_interval()
+        elif is_comparison(optree):
+            return evaluate_comparison_range(optree)
+        else:
+            args_interval = tuple(
+                evaluate_range(op) for op in
+                optree.get_inputs()
+            )
+            return optree.apply_bare_range_function(args_interval)
