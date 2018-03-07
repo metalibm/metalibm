679099f110401e5d1b7306af9a8452f2c1ba0979
Nicolas Brunie (Kalray) <nbrunie@kalray.eu>
2018-03-07 21:54:32 +0100

    Cleaning headers in metalibm_core.utility module

--- metalibm_core/utility/version_info.py
+++ metalibm_core/utility/version_info.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
+###############################################################################
+# This file is part of metalibm (https://github.com/kalray/metalibm)
+###############################################################################
+# MIT License
+#
+# Copyright (c) 2018 Kalray
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###############################################################################
 # Version history:
-#  0.1d: adding vhdl support (backend, code generation,
+# - 1.0: cleaning headers
+# - ....
+# - 0.1d: adding vhdl support (backend, code generation,
 #        ml_entity and code_entity)
-#
-#
+# - ....
+###############################################################################
 
 """ Version information for Metalibm """
 
@@ -25,6 +52,6 @@ def extract_git_hash():
     return git_sha
 
 GIT_SHA = extract_git_hash()
-VERSION_NUM = "0.1d"
+VERSION_NUM = "1.0"
 VERSION_DESCRIPTION = "alpha"
 NOTES = """ metalibm core """
