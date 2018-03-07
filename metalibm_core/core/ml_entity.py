e1acbd5b65e45899bc0f207da72a6b7d1bc72b2e
Nicolas Brunie (Kalray) <nbrunie@kalray.eu>
2018-03-07 20:57:38 +0100

    Cleaning header in metalibm.core module

--- metalibm_core/core/ml_entity.py
+++ metalibm_core/core/ml_entity.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 
 ###############################################################################
-# This file is part of New Metalibm tool
-# Copyright (2016-)
-# All rights reserved
-# created:          Nov 17th, 2016
-# last-modified:    Mar 06th, 2018
+# This file is part of metalibm (https://github.com/kalray/metalibm)
+###############################################################################
+# MIT License
 #
-# author(s):   Nicolas Brunie (nibrunie@gmail.com)
+# Copyright (c) 2018 Kalray
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###############################################################################
+
+###############################################################################
+# created:          Nov 17th, 2016
+# last-modified:    Mar  8th, 2018
+#
+# author(s):   Nicolas Brunie (nbrunie@kalray.eu)
 # decription:  Declare and implement a class to manage
 #              hardware (vhdl like) entities
 ###############################################################################
--- metalibm_core/core/special_values.py
+++ metalibm_core/core/special_values.py
@@ -1,17 +1,39 @@
 # -*- coding: utf-8 -*-
 
-## @package special_values
-#  Metalibm Formats special_values
 
 ###############################################################################
-# This file is part of the New Metalibm tool
-# Copyright (2017-)
-# All rights reserved
-# created:          Aug 20th, 2017
-# last-modified:    Aug 20th, 2017
-#
-# author(s): Nicolas Brunie (nibrunie@gmail.com)
+# This file is part of metalibm (https://github.com/kalray/metalibm)
 ###############################################################################
+# MIT License
+#
+# Copyright (c) 2018 Kalray
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###############################################################################
+# created:          Aug 20th, 2017
+# last-modified:    Mar  7th, 2018
+#
+# author(s): Nicolas Brunie (nbrunie@kalray.eu)
+###############################################################################
+
+## @package special_values
+#  Metalibm Formats special_values
 
 import sollya
 from sollya import S2
@@ -35,7 +57,7 @@ class NumericValue(sollya.SollyaObject):
         else:
             return NumericValue(sollya.SollyaObject.__sub__(lhs, rhs))
 
-            
+
 ###############################################################################
 #                     FLOATING-POINT SPECIAL VALUES
 ###############################################################################
