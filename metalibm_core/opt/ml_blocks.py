0cafcef1651910cddc6fd5e6eda359ca6385b9f3
Nicolas Brunie (Kalray) <nbrunie@kalray.eu>
2018-03-07 21:43:21 +0100

    Cleaning headers in metalibm_core.opt

--- metalibm_core/opt/ml_blocks.py
+++ metalibm_core/opt/ml_blocks.py
@@ -1,11 +1,36 @@
 # -*- coding: utf-8 -*-
 # vim: sw=2 sts=2
 
+###############################################################################
+# This file is part of metalibm (https://github.com/kalray/metalibm)
+###############################################################################
+# MIT License
+#
+# Copyright (c) 2018 Kalray
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###############################################################################
 # Meta-blocks that return an optree to be used in an optimization pass
-# Created:        Fri Apr 28 15:04:25 CEST 2017
-# Last modified:  Fri May  5 11:09:17 CEST 2017
-# Contributors:
-#   Hugues de Lassus <hugues.de-lassus@univ-perp.fr>
+# Created:          Apr 28th, 2017
+# last-modified:    Mar  7th, 2018
+# Author(s):  Hugues de Lassus <hugues.de-lassus@univ-perp.fr>
+###############################################################################
 
 
 from metalibm_core.core.ml_operations import (
--- metalibm_core/opt/rtl_fixed_point_utils.py
+++ metalibm_core/opt/rtl_fixed_point_utils.py
@@ -2,14 +2,35 @@
 """ Utilities to manipulate, adapt and check fixed-point precisions """
 
 ###############################################################################
-# This file is part of New Metalibm tool
-# Copyrights Nicolas Brunie (2017-)
-# All rights reserved
-# created:          Jul  8th, 2017
-# last-modified:    Jul  8th, 2017
+# This file is part of metalibm (https://github.com/kalray/metalibm)
+###############################################################################
+# MIT License
 #
-# author(s): Nicolas Brunie (nibrunie@gmail.com)
-# description: Utilities for fixed-point node manipulation
+# Copyright (c) 2018 Kalray
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###############################################################################
+# Created:          Jul  8th, 2017
+# Last-modified:    Mar  7th, 2018
+#
+# Author(s): Nicolas Brunie (nbrunie@kalray.eu)
+# Description: Utilities for fixed-point node manipulation
 ###############################################################################
 
 # python3 legacy compatibility
