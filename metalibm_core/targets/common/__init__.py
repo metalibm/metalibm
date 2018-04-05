4e5b4fc8d1bd7028b2ac8f83a7c81700e385f776
Nicolas Brunie <nbrunie@kalray.eu>
2018-04-05 23:32:16 +0200

    Extending ML_FunctionBasis to be able to generate LLVM-IR

--- metalibm_core/targets/common/__init__.py
+++ metalibm_core/targets/common/__init__.py
@@ -28,3 +28,4 @@
 
 from .fixed_point_backend import FixedPointBackend
 from .vector_backend      import VectorBackend
+from .llvm_ir import LLVMBackend
--- metalibm_core/utility/ml_template.py
+++ metalibm_core/utility/ml_template.py
@@ -104,6 +104,8 @@ language_map = {
     "opencl": OpenCL_Code,
     "gappa": Gappa_Code,
     "vhdl": VHDL_Code,
+
+    "ll": LLVM_IR_Code,
 }
 
 
@@ -205,7 +207,10 @@ def target_instanciate(target_name):
 
 def language_parser(language_str):
     """ string -> Language object conversion """
-    return language_map[language_str]
+    if not language_str in language_map:
+        Log.report(Log.Error, "unknown language %s" % language_str)
+    else:
+        return language_map[language_str]
 
 
 class DisplayExceptionAction(argparse.Action):
