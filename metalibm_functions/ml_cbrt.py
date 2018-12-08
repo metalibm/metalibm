cc05f7d45261183cd505a4a653e6eab4cde2c94d
Nicolas Brunie <nibrunie@gmail.com>
2018-12-08 19:00:48 +0100

    Cleaning metalibm_functions.ml_cbrt
    Details:
    *    + fixing indentation level to follow PEP8
    *    + fixing import lists

--- metalibm_functions/ml_cbrt.py
+++ metalibm_functions/ml_cbrt.py
@@ -32,11 +32,15 @@ import sys
 
 import sollya
 
-from sollya import S2, Interval, ceil, floor, round, inf, sup, log, exp, expm1, log2, guessdegree, dirtyinfnorm, RN, RD
+from sollya import (
+    round, RN, RD
+)
+
 try:
     from sollya import cbrt
 except ImportError:
     from sollya_extra_functions import cbrt
+
 from sollya import parse as sollya_parse
 
 from metalibm_core.core.attributes import ML_Debug
@@ -46,7 +50,9 @@ from metalibm_core.core.ml_table import ML_NewTable
 from metalibm_core.code_generation.generic_processor import GenericProcessor
 from metalibm_core.core.polynomials import *
 from metalibm_core.core.ml_function import ML_FunctionBasis, DefaultArgTemplate
-from metalibm_core.code_generation.generator_utility import FunctionOperator, FO_Result, FO_Arg
+from metalibm_core.code_generation.generator_utility import (
+    FunctionOperator, FO_Result, FO_Arg
+)
 from metalibm_core.core.ml_complex_formats import ML_Mpfr_t
 
 
@@ -59,168 +65,156 @@ from metalibm_core.utility.gappa_utils import is_gappa_installed
 
 
 class ML_Cbrt(ML_FunctionBasis):
-  function_name = "ml_cbrt"
-  def __init__(self, args=DefaultArgTemplate):
-    # initializing base class
-    ML_FunctionBasis.__init__(self, args)
+    function_name = "ml_cbrt"
+    def __init__(self, args=DefaultArgTemplate):
+        # initializing base class
+        ML_FunctionBasis.__init__(self, args)
 
 
-  @staticmethod
-  def get_default_args(**kw):
-    """ Return a structure containing the arguments for ML_Cbrt,
-        builtin from a default argument mapping overloaded with @p kw """
-    default_args_cbrt = {
-        "output_file": "my_cbrt.c",
-        "function_name": "my_cbrt",
-        "precision": ML_Binary32,
-        "accuracy": ML_Faithful,
-        "target": GenericProcessor()
-    }
-    default_args_cbrt.update(kw)
-    return DefaultArgTemplate(**default_args_cbrt)
+    @staticmethod
+    def get_default_args(**kw):
+        """ Return a structure containing the arguments for ML_Cbrt,
+                builtin from a default argument mapping overloaded with @p kw """
+        default_args_cbrt = {
+                "output_file": "my_cbrt.c",
+                "function_name": "my_cbrt",
+                "precision": ML_Binary32,
+                "accuracy": ML_Faithful,
+                "target": GenericProcessor()
+        }
+        default_args_cbrt.update(kw)
+        return DefaultArgTemplate(**default_args_cbrt)
 
-  def generate_scheme(self):
-    # declaring main input variable
-    vx = self.implementation.add_input_variable("x", self.precision) 
+    def generate_scheme(self):
+        # declaring main input variable
+        vx = self.implementation.add_input_variable("x", self.precision) 
 
-    # declaring approximation parameters
-    index_size = 6
-    num_iteration = 8
+        # declaring approximation parameters
+        index_size = 6
+        num_iteration = 8
 
-    Log.set_dump_stdout(True)
+        Log.set_dump_stdout(True)
 
-    Log.report(Log.Info, "\033[33;1m generating implementation scheme \033[0m")
-    if self.debug_flag: 
-        Log.report(Log.Info, "\033[31;1m debug has been enabled \033[0;m")
+        Log.report(Log.Info, "\033[33;1m generating implementation scheme \033[0m")
+        if self.debug_flag: 
+                Log.report(Log.Info, "\033[31;1m debug has been enabled \033[0;m")
 
-    # local overloading of RaiseReturn operation
-    def ExpRaiseReturn(*args, **kwords):
-        kwords["arg_value"] = vx
-        kwords["function_name"] = self.function_name
-        return RaiseReturn(*args, **kwords)
-
-
-
-    def cbrt_newton_iteration(current_approx, input_value, input_inverse):
-      # Cubic root of A is approximated by a Newton-Raphson iteration
-      # on f(x) = 1 - A / x^3
-      # x_n+1 = 4/3 * x_n - x_n^4 / (3 * A)
-      # x_n+1 = 1/3 * (x_n * (1 - x_n^3/A) + x_n)
-
-      approx_triple = Multiplication(current_approx, Multiplication(current_approx, current_approx))
-
-      diff      = FMSN(approx_triple, input_inverse, Constant(1, precision = self.precision))
-      injection = FMA(
-        Multiplication(
-          current_approx, 
-          Constant(1/3.0, precision = self.precision),
-        ),
-        diff, current_approx)
-
-      new_approx = injection
-
-      return new_approx
+        # local overloading of RaiseReturn operation
+        def ExpRaiseReturn(*args, **kwords):
+                kwords["arg_value"] = vx
+                kwords["function_name"] = self.function_name
+                return RaiseReturn(*args, **kwords)
 
 
-    reduced_vx = MantissaExtraction(vx, precision = self.precision)
 
-    int_precision = self.precision.get_integer_format()
+        def cbrt_newton_iteration(current_approx, input_value, input_inverse):
+            # Cubic root of A is approximated by a Newton-Raphson iteration
+            # on f(x) = 1 - A / x^3
+            # x_n+1 = 4/3 * x_n - x_n^4 / (3 * A)
+            # x_n+1 = 1/3 * (x_n * (1 - x_n^3/A) + x_n)
+
+            approx_triple = Multiplication(current_approx, Multiplication(current_approx, current_approx))
+
+            diff            = FMSN(approx_triple, input_inverse, Constant(1, precision = self.precision))
+            injection = FMA(
+                Multiplication(
+                    current_approx, 
+                    Constant(1/3.0, precision = self.precision),
+                ),
+                diff, current_approx)
+
+            new_approx = injection
+
+            return new_approx
 
 
-    cbrt_approx_table = ML_NewTable(dimensions = [2**index_size, 1], storage_precision = self.precision, tag = self.uniquify_name("cbrt_approx_table"))
-    for i in range(2**index_size):
-      input_value = 1 + i / SollyaObject(2**index_size) 
+        reduced_vx = MantissaExtraction(vx, precision = self.precision)
 
-      cbrt_approx = cbrt(input_value)
-      cbrt_approx_table[i][0] = round(cbrt_approx, self.precision.get_sollya_object(), RN)
+        int_precision = self.precision.get_integer_format()
 
-    # Modulo operations will returns a reduced exponent within [-3, 2]
-    # so we approximate cbrt on this interval (with index offset by -3)
-    cbrt_mod_table = ML_NewTable(dimensions = [6, 1], storage_precision = self.precision, tag = self.uniquify_name("cbrt_mod_table"))
-    for i in range(6):
-      input_value = SollyaObject(2)**(i-3)
-      cbrt_mod_table[i][0] = round(cbrt(input_value), self.precision.get_sollya_object(), RN)
 
-    vx_int = TypeCast(reduced_vx, precision = int_precision)
-    mask = BitLogicRightShift(vx_int, self.precision.get_precision() - index_size, precision = int_precision)
-    mask = BitLogicAnd(mask, Constant(2**index_size - 1, precision = int_precision), precision = int_precision, tag = "table_index", debug=debug_multi)
-    table_index = mask
+        cbrt_approx_table = ML_NewTable(dimensions = [2**index_size, 1], storage_precision = self.precision, tag = self.uniquify_name("cbrt_approx_table"))
+        for i in range(2**index_size):
+            input_value = 1 + i / SollyaObject(2**index_size) 
 
-    exp_vx = ExponentExtraction(vx, precision = ML_Int32, tag = "exp_vx")
-    exp_vx_third = Division(exp_vx, Constant(3, precision = ML_Int32), precision = ML_Int32, tag = "exp_vx_third")
-    exp_vx_mod   = Modulo(exp_vx, Constant(3, precision = ML_Int32), precision = ML_Int32, tag = "exp_vx_mod", debug=debug_multi)
+            cbrt_approx = cbrt(input_value)
+            cbrt_approx_table[i][0] = round(cbrt_approx, self.precision.get_sollya_object(), RN)
 
-    # offset on modulo to make sure table index is positive
-    exp_vx_mod = exp_vx_mod + 3
+        # Modulo operations will returns a reduced exponent within [-3, 2]
+        # so we approximate cbrt on this interval (with index offset by -3)
+        cbrt_mod_table = ML_NewTable(dimensions = [6, 1], storage_precision = self.precision, tag = self.uniquify_name("cbrt_mod_table"))
+        for i in range(6):
+            input_value = SollyaObject(2)**(i-3)
+            cbrt_mod_table[i][0] = round(cbrt(input_value), self.precision.get_sollya_object(), RN)
 
-    cbrt_mod = TableLoad(cbrt_mod_table, exp_vx_mod, Constant(0), tag = "cbrt_mod")
+        vx_int = TypeCast(reduced_vx, precision = int_precision)
+        mask = BitLogicRightShift(vx_int, self.precision.get_precision() - index_size, precision = int_precision)
+        mask = BitLogicAnd(mask, Constant(2**index_size - 1, precision = int_precision), precision = int_precision, tag = "table_index", debug=debug_multi)
+        table_index = mask
 
-    init_approx = Multiplication(
-      Multiplication(
-        # approx cbrt(mantissa)
-        TableLoad(cbrt_approx_table, table_index, Constant(0, precision = ML_Int32), tag="seed", debug=debug_multi),
-        # approx cbrt(2^(e%3))
-        cbrt_mod,
-        tag="init_mult", 
-        debug=debug_multi,
-        precision=self.precision
-      ),
-      # 2^(e/3)
-      ExponentInsertion(exp_vx_third, precision = self.precision, tag="exp_vx_third", debug=debug_multi),
-      tag="init_approx",
-      debug=debug_multi,
-      precision = self.precision
-    )
+        exp_vx = ExponentExtraction(vx, precision = ML_Int32, tag = "exp_vx")
+        exp_vx_third = Division(exp_vx, Constant(3, precision = ML_Int32), precision = ML_Int32, tag = "exp_vx_third")
+        exp_vx_mod     = Modulo(exp_vx, Constant(3, precision = ML_Int32), precision = ML_Int32, tag = "exp_vx_mod", debug=debug_multi)
 
-    inverse_red_vx = Division(Constant(1, precision = self.precision), reduced_vx)
-    inverse_vx = Division(Constant(1, precision = self.precision), vx)
+        # offset on modulo to make sure table index is positive
+        exp_vx_mod = exp_vx_mod + 3
 
-    current_approx = init_approx
+        cbrt_mod = TableLoad(cbrt_mod_table, exp_vx_mod, Constant(0), tag = "cbrt_mod")
 
-    for i in range(num_iteration):
-      #current_approx = cbrt_newton_iteration(current_approx, reduced_vx, inverse_red_vx) 
-      current_approx = cbrt_newton_iteration(current_approx, vx, inverse_vx) 
-      current_approx.set_attributes(tag="approx_%d" % i, debug=debug_multi)
+        init_approx = Multiplication(
+            Multiplication(
+                # approx cbrt(mantissa)
+                TableLoad(cbrt_approx_table, table_index, Constant(0, precision = ML_Int32), tag="seed", debug=debug_multi),
+                # approx cbrt(2^(e%3))
+                cbrt_mod,
+                tag="init_mult", 
+                debug=debug_multi,
+                precision=self.precision
+            ),
+            # 2^(e/3)
+            ExponentInsertion(exp_vx_third, precision = self.precision, tag="exp_vx_third", debug=debug_multi),
+            tag="init_approx",
+            debug=debug_multi,
+            precision = self.precision
+        )
 
-    result = current_approx
-    result.set_attributes(tag="result", debug=debug_multi)
+        inverse_red_vx = Division(Constant(1, precision = self.precision), reduced_vx)
+        inverse_vx = Division(Constant(1, precision = self.precision), vx)
 
-    # last iteration
-    ext_precision = ML_DoubleDouble
-    xn_2 = Multiplication(current_approx, current_approx, precision = ext_precision)
-    xn_3 = Multiplication(current_approx, xn_2, precision = ext_precision)
+        current_approx = init_approx
 
-    FourThird = Constant(4/SollyaObject(3), precision = ext_precision)
+        for i in range(num_iteration):
+            #current_approx = cbrt_newton_iteration(current_approx, reduced_vx, inverse_red_vx) 
+            current_approx = cbrt_newton_iteration(current_approx, vx, inverse_vx) 
+            current_approx.set_attributes(tag="approx_%d" % i, debug=debug_multi)
 
-    # main scheme
-    Log.report(Log.Info, "\033[33;1m MDL scheme \033[0m")
-    scheme = Statement(
-                Return(
-                    result
-                  ))
+        result = current_approx
+        result.set_attributes(tag="result", debug=debug_multi)
 
-    return scheme
+        # last iteration
+        ext_precision = ML_DoubleDouble
+        xn_2 = Multiplication(current_approx, current_approx, precision = ext_precision)
+        xn_3 = Multiplication(current_approx, xn_2, precision = ext_precision)
 
-  def generate_emulate(self, result_ternary, result, mpfr_x, mpfr_rnd):
-    """ generate the emulation code for ML_Log2 functions
-        mpfr_x is a mpfr_t variable which should have the right precision
-        mpfr_rnd is the rounding mode
-    """
-    emulate_func_name = "mpfr_cbrt"
-    emulate_func_op = FunctionOperator(emulate_func_name, arg_map = {0: FO_Arg(0), 1: FO_Arg(1), 2: FO_Arg(2)}, require_header = ["mpfr.h"]) 
-    emulate_func   = FunctionObject(emulate_func_name, [ML_Mpfr_t, ML_Mpfr_t, ML_Int32], ML_Int32, emulate_func_op)
-    mpfr_call = Statement(ReferenceAssign(result_ternary, emulate_func(result, mpfr_x, mpfr_rnd)))
+        FourThird = Constant(4/SollyaObject(3), precision = ext_precision)
 
-    return mpfr_call
+        # main scheme
+        Log.report(Log.Info, "\033[33;1m MDL scheme \033[0m")
+        scheme = Statement(
+                                Return(
+                                        result
+                                    ))
 
-  def numeric_emulate(self, input_value):
-    return cbrt(input_value)
+        return scheme
 
-  standard_test_cases =[
-    (sollya_parse(x),) for x in  ["1.1", "1.5"]
-  ] + [
-    (sollya.parse("0x1.4fc9bcp-1"),),
-  ]
+    def numeric_emulate(self, input_value):
+        return cbrt(input_value)
+
+    standard_test_cases =[
+        (sollya_parse(x),) for x in    ["1.1", "1.5"]
+    ] + [
+        (sollya.parse("0x1.4fc9bcp-1"),),
+    ]
 
 
 if __name__ == "__main__":
