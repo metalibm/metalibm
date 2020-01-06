a14cf3221cb7bef91dbd29fd9798eeee53ed97cf
Nicolas Brunie <nibrunie@gmail.com>
2020-01-06 08:55:16 +0100

    [bugfix] fixing RTL tool selection in hw_non_regression
    Details:
    * "ghdl" is the RTL simulator currently used in CI, fixing valid.hw_non_regression
      to specifically use this tool for elab and simulation valid

--- valid/hw_non_regression.py
+++ valid/hw_non_regression.py
@@ -95,14 +95,14 @@ new_scheme_function_list = [
     "floating-point division",
     metalibm_hw_blocks.ml_fp_div.FP_Divider,
     [
-        {"build_enable": True},
+        {"build_enable": True, "simulator": "ghdl"},
     ]
   ),
   EntitySchemeTest(
     "bipartite approximation operator",
     metalibm_hw_blocks.bipartite_approx.BipartiteApprox,
     [
-        {"build_enable": True},
+        {"build_enable": True, "simulator": "ghdl"},
     ]
   ),
   EntitySchemeTest(
@@ -119,24 +119,24 @@ new_scheme_function_list = [
     "multiplication array",
     mult_array.MultArray,
     [
-        {"op_expr": mult_array_parser("FS9.0xFS9.0+FU13.0"), "build_enable": True},
-        {"dummy_mode": True, "method": mult_array.ReductionMethod.Wallace, "build_enable": True},
+        {"op_expr": mult_array_parser("FS9.0xFS9.0+FU13.0"), "build_enable": True, "simulator": "ghdl"},
+        {"dummy_mode": True, "method": mult_array.ReductionMethod.Wallace, "build_enable": True, "simulator": "ghdl"},
         {"booth_mode": True, "method": mult_array.ReductionMethod.Dadda_4to2,
-         "op_expr": mult_array_parser("FS9.4xFS9.-2+FU13.3") , "build_enable": True},
+         "op_expr": mult_array_parser("FS9.4xFS9.-2+FU13.3") , "build_enable": True, "simulator": "ghdl"},
         {"dummy_mode": True, "method": mult_array.ReductionMethod.Wallace_4to2,
-         "op_expr": mult_array_parser("FS9.4xFS9.-2+FU13.3xFS3.3") , "build_enable": True},
-        {"booth_mode": True, "method": mult_array.ReductionMethod.Dadda, "build_enable": True},
+         "op_expr": mult_array_parser("FS9.4xFS9.-2+FU13.3xFS3.3") , "build_enable": True, "simulator": "ghdl"},
+        {"booth_mode": True, "method": mult_array.ReductionMethod.Dadda, "build_enable": True, "simulator": "ghdl"},
         # covering tag option
         {"dummy_mode": True, "method": mult_array.ReductionMethod.Wallace_4to2,
          "pipelined": True,
-         "op_expr": mult_array_parser("FS9.4xFS9.-2[0,mytag]+FU13.3[1,other_tag]") , "build_enable": True},
+         "op_expr": mult_array_parser("FS9.4xFS9.-2[0,mytag]+FU13.3[1,other_tag]") , "build_enable": True, "simulator": "ghdl"},
     ]
   ),
   EntitySchemeTest(
     "compound adder",
     compound_adder.CompoundAdder,
     [
-        {},
+        {"auto_test": 10, "execute_trigger": True, "simulator": "ghdl"},
     ]
   ),
 ]
