5aad43cd4d689078d67cd3125aa80acb5e212255
Nicolas Brunie <nibrunie@gmail.com>
2020-02-02 10:38:01 +0100

    Extending insert_line_break to specify start of line break

--- metalibm_core/code_generation/code_utility.py
+++ metalibm_core/code_generation/code_utility.py
@@ -34,17 +34,23 @@
 ###############################################################################
 
 
-def insert_line_break(full_line, break_char=" \\\n    ", sep=" ", break_len=80):
+def insert_line_break(full_line, break_char=" \\\n    ", sep=" ", break_len=80, allow_init_break=True):
     """ Break a string into multiple lines
         @param full_line line to be broken
         @param break_char the sub-string inserted when the input line is broken
         @param sep line-breaking is only possible on a sep sub-string
         @param break_len maximal size allowed for an unbroken line
+        @param allow_init_break insert break_char even at the start
+                                even if 1st sub-string is too long
         @return new string containing broken down version of @p full_line """
     lexem_list = full_line.split(sep)
     result_line = ""
     sub_line = ""
-    for lexem in lexem_list:
+    if len(lexem_list[0]) > break_len and allow_init_break:
+        sub_line = sep + lexem_list[0]
+    else:
+        sub_line = lexem_list[0]
+    for lexem in lexem_list[1:]:
         if len(sub_line) + len(lexem) + len(sep) > break_len:
             result_line = result_line + sub_line + break_char
             sub_line = lexem
