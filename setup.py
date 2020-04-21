cc1a45ba7001aac80e1132a1b9d000043c2be760
Nicolas Brunie <nibrunie@gmail.com>
2020-04-21 21:55:51 +0200

    adding setup.py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="metalibm", # Replace with your own username
+    version="1.0.0",
+    author="Nicolas Brunie",
+    author_email="nbrunie@kalray.eu",
+    description="a math kernel code generation",
+    url="https://github.com/kalray/metalibm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
