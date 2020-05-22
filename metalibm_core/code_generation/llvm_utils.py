4ac35f4fe46c1a1e7ff39f5a883be47da014507d
Nicolas Brunie <nibrunie@gmail.com>
2020-05-22 13:16:34 +0200

    Adding support for ML_Pointer_Format in llvm_ir_format

--- metalibm_core/code_generation/llvm_utils.py
+++ metalibm_core/code_generation/llvm_utils.py
@@ -37,35 +37,44 @@ from metalibm_core.core.ml_formats import (
     v2int32, v2int64, v2float32, v2float64,
     v8int32, v8int64, v8float32, v8float64,
     ML_Bool, v2bool, v4bool, v8bool,
+    ML_Void,
 )
+from metalibm_core.core.ml_complex_formats import is_pointer_format
 from metalibm_core.utility.log_report import Log
+
+LLVM_IR_DATA_FORMAT_MAP = {
+    ML_Void: "void",
+    ML_Bool: "i1",
+    v2bool: "<2 x i1>",
+    v4bool: "<4 x i1>",
+    v8bool: "<8 x i1>",
+    ML_Int32: "i32",
+    ML_Int64: "i64",
+    ML_Binary32: "float",
+    ML_Binary64: "double",
+    v2int32: "<2 x i32>",
+    v2int64: "<2 x i64>",
+    v2float32: "<2 x float>",
+    v2float64: "<2 x double>",
+    v4float32: "<4 x float>",
+    v4float64: "<4 x double>",
+    v4int32: "<4 x i32>",
+    v4int64: "<4 x i64>",
+    v8int32: "<8 x i32>",
+    v8float32: "<8 x float>",
+    v8int64: "<8 x i64>",
+    v8float64: "<8 x double>",
+
+    ML_Int128: "i128",
+    ML_Int256: "i256",
+}
+
+
 def llvm_ir_format(precision):
     """ Translate from Metalibm precision to string for LLVM-IR format """
-    try:
-        return {
-            ML_Bool: "i1",
-            v2bool: "<2 x i1>",
-            v4bool: "<4 x i1>",
-            v8bool: "<8 x i1>",
-            ML_Int32: "i32",
-            ML_Int64: "i64",
-            ML_Binary32: "float",
-            ML_Binary64: "double",
-            v2int32: "<2 x i32>",
-            v2int64: "<2 x i64>",
-            v2float32: "<2 x float>",
-            v2float64: "<2 x double>",
-            v4float32: "<4 x float>",
-            v4float64: "<4 x double>",
-            v4int32: "<4 x i32>",
-            v4int64: "<4 x i64>",
-            v8int32: "<8 x i32>",
-            v8float32: "<8 x float>",
-            v8int64: "<8 x i64>",
-            v8float64: "<8 x double>",
-
-            ML_Int128: "i128",
-            ML_Int256: "i256",
-        }[precision]
-    except KeyError:
+    if is_pointer_format(precision):
+        return "{}*".format(llvm_ir_format(precision.get_data_precision()))
+    elif precision in LLVM_IR_DATA_FORMAT_MAP:
+        return LLVM_IR_DATA_FORMAT_MAP[precision]
+    else:
         Log.report(Log.Error, "unknown precision {} in llvm_ir_format".format(precision), error=KeyError)
