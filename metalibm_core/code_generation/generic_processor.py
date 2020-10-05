dd0f6616bddb241e294a5fd33f65b07923a1f219
Nicolas Brunie <nbrunie@kalray.eu>
2020-10-05 21:14:49 +0200

    Adding generic implementation for Modulo ML_UInt64

--- metalibm_core/code_generation/generic_processor.py
+++ metalibm_core/code_generation/generic_processor.py
@@ -531,7 +531,7 @@ c_code_generation_table = {
         None: build_simplified_operator_generation([ML_Int64, ML_Int32, ML_UInt32, ML_Binary32, ML_Binary64], 2, SymbolOperator("/", arity = 2)),
     },
     Modulo: {
-        None: build_simplified_operator_generation([ML_Int32, ML_UInt32, ML_Int64], 2, SymbolOperator("%", arity = 2)),
+        None: build_simplified_operator_generation([ML_Int32, ML_UInt32, ML_Int64, ML_UInt64], 2, SymbolOperator("%", arity = 2)),
     },
     Comparison: 
         dict (
