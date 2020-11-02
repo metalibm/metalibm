e8662336603069077eb10f658edaf702587db101
Nicolas Brunie <nbrunie@kalray.eu>
2020-11-02 22:14:14 +0100

    [fix] Fixing display utils for vector format

--- metalibm_core/core/display_utils.py
+++ metalibm_core/core/display_utils.py
@@ -100,9 +100,9 @@ DISPLAY_DS      = DisplayFormat(format_string="{{.hi=%a, .lo=%a}}", pre_process_
 DISPLAY_TD      = DisplayFormat(format_string="{{.hi=%a, .me=%a, .lo=%a}}", pre_process_fct= lambda v: "%s.hi, %s.me, %s.lo" % (v, v, v))
 DISPLAY_TS      = DisplayFormat(format_string="{{.hi=%a, .me=%a, .lo=%a}}", pre_process_fct= lambda v : "%s.hi, %s.me, %s.lo" % (v, v, v))
 
-DISPLAY_float2  = DisplayFormat(format_string="[%a, %a]", pre_process_fct = lambda v: "%s._[0], %s._[1]" % (v, v))
-DISPLAY_float4  = DisplayFormat(format_string="[%a, %a, %a, %a]", pre_process_fct = lambda v: "%s._[0], %s._[1], %s._[2], %s._[3]" % (v, v, v, v))
-DISPLAY_float8  = DisplayFormat(format_string="[a, %a, %a, %a, %a, %a, %a, %a]", pre_process_fct = lambda v: "%s._[0], %s._[1], %s._[2], %s._[3], %s._[4], %s._[5], %s._[6], %s._[7]" % (v, v, v, v, v, v, v, v))
+DISPLAY_float2  = DisplayFormat(format_string="[%a, %a]", pre_process_fct = lambda v: "%s[0], %s[1]" % (v, v))
+DISPLAY_float4  = DisplayFormat(format_string="[%a, %a, %a, %a]", pre_process_fct = lambda v: "%s[0], %s[1], %s[2], %s[3]" % (v, v, v, v))
+DISPLAY_float8  = DisplayFormat(format_string="[%a, %a, %a, %a, %a, %a, %a, %a]", pre_process_fct = lambda v: "%s[0], %s[1], %s[2], %s[3], %s[4], %s[5], %s[6], %s[7]" % (v, v, v, v, v, v, v, v))
 
 DISPLAY_int2  = DisplayFormat(format_string="[%d, %d]", pre_process_fct = lambda v: "%s._[0], %s._[1]" % (v, v))
 DISPLAY_int4  = DisplayFormat(format_string="[%d, %d, %d, %d]", pre_process_fct = lambda v: "%s._[0], %s._[1], %s._[2], %s._[3]" % (v, v, v, v))
