91d63b20571131f1468a0705e0b404e54776edb1
Nicolas Brunie <nbrunie@kalray.eu>
2021-01-28 17:38:13 +0100

    Prototyping rounding implementation for RTL dequantizer

--- metalibm_hw_blocks/dequantizer.py
+++ metalibm_hw_blocks/dequantizer.py
@@ -74,6 +74,13 @@ def fixed_normalized_mantissa(op):
     )
 
 FIX32 = fixed_point(32, 0, signed=True)
+ROUNDING_MODE_FORMAT = ML_StdLogicVectorFormat(3)
+
+ROUND_RNE = Constant(0, precision=ROUNDING_MODE_FORMAT)
+ROUND_RU = Constant(1, precision=ROUNDING_MODE_FORMAT)
+ROUND_RD = Constant(2, precision=ROUNDING_MODE_FORMAT)
+ROUND_RZ = Constant(3, precision=ROUNDING_MODE_FORMAT)
+ROUND_RAZ = Constant(4, precision=ROUNDING_MODE_FORMAT)
 
 class Dequantizer(ML_Entity("dequantizer")):
     """ Implement the post-processing operator for
@@ -122,6 +129,7 @@ class Dequantizer(ML_Entity("dequantizer")):
         scale = self.implementation.add_input_variable("scale", scale_format)
         quantized_input = self.implementation.add_input_variable("quantized_input", quantized_input_format)
         offset_input = self.implementation.add_input_variable("offset", offset_input_format)
+        rounding_mode = self.implementation.add_input_variable("round_mode", ROUNDING_MODE_FORMAT)
 
         support_format = self.precision.get_support_format()
         base_format = self.precision.get_base_format()
@@ -162,17 +170,45 @@ class Dequantizer(ML_Entity("dequantizer")):
         offseted_field = scaled_field + Conversion(offset_input, precision=fixed_point(offset_input_format.get_bit_size(), 0), tag="extended_offset", debug=debug_std)
         offseted_field.set_attributes(tag="offseted_field", debug=debug_std)
 
-        round_bit = BitSelection(offseted_field, FixedPointPosition(offseted_field, -1, align=FixedPointPosition.FromPointToLSB))
-        sticky_bit = NotEqual(SubSignalSelection(offseted_field, 0, FixedPointPosition(offseted_field, -2, align=FixedPointPosition.FromPointToLSB)), 0)
+        round_bit = Equal(
+            BitSelection(offseted_field, FixedPointPosition(offseted_field, -1, align=FixedPointPosition.FromPointToLSB), tag="round_bit"),
+            Constant(0, precision=ML_StdLogic))
+        sticky_bit = NotEqual(SubSignalSelection(offseted_field, 0, FixedPointPosition(offseted_field, -2, align=FixedPointPosition.FromPointToLSB), tag="sticky_bitfield", precision=None), 0, tag="sticky_bit")
+
+        offseted_field_negative = offseted_field < 0
+        offseted_field_parity_bit = BitSelection(offseted_field, FixedPointPosition(offseted_field, 0, align=FixedPointPosition.FromPointToLSB), tag="parity_bit")
+
+        offseted_field_even = Equal(offseted_field_parity_bit, Constant(0, precision=ML_StdLogic), tag="offseted_field_even")
 
         # TODO: implement rounding
+        # increment if round-up and (round_bit or sticky_bit)
+        #           if round-rz and (result negative) and (round_bit or sticky_bit)
+        #           if round-rne and (round_bit and (sticky_bit or (not result even)))
+        #           if round-raz and (round_bit or sticky_bit and (result positive))
+        round_up = Equal(rounding_mode, ROUND_RU)
+        round_rz = Equal(rounding_mode, ROUND_RZ)
+        round_rne = Equal(rounding_mode, ROUND_RNE)
+        round_down = Equal(rounding_mode, ROUND_RD)
+        round_raz = Equal(rounding_mode, ROUND_RAZ)
+        round_increment = Select(
+            logical_or_reduce([
+                LogicalAnd(round_up, LogicalOr(round_bit, sticky_bit)),
+                LogicalAnd(round_rz, LogicalAnd(offseted_field_negative, LogicalOr(round_bit, sticky_bit))),
+                LogicalAnd(round_rne, LogicalAnd(round_bit, LogicalOr(sticky_bit, offseted_field_even))),
+                LogicalAnd(round_raz, LogicalAnd(LogicalOr(round_bit, sticky_bit), LogicalNot(offseted_field_negative)))
+            ]),
+            1,
+            0,
+            precision=fixed_point(1, 0, signed=False),
+            tag="round_increment")
 
+        rounded_field = offseted_field + round_increment
         result_format = self.get_io_format("result")
 
         # detecting overflow / underflow
         MAX_BOUND = self.get_io_format("result").get_max_value()
         MIN_BOUND = self.get_io_format("result").get_min_value()
-        bounded_result = Max(MIN_BOUND, Min(offseted_field, MAX_BOUND))
+        bounded_result = Max(MIN_BOUND, Min(rounded_field, MAX_BOUND))
 
         result = Conversion(bounded_result, precision=result_format)
 
