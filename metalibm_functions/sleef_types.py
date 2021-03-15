042bf06b90d097f4a8e357b0970c927b99d30fd1
Nicolas Brunie <nbrunie@kalray.eu>
2021-03-15 14:04:09 +0100

    Fixing sleef vector types parent class

--- metalibm_functions/sleef_types.py
+++ metalibm_functions/sleef_types.py
@@ -38,7 +38,8 @@
 import sollya
 
 from metalibm_core.core.ml_formats import (
-    ML_Compound_Format, v4float64, ML_String, ML_Binary64)
+    ML_Compound_Format, ML_CompoundVectorFormat, ML_VectorFormat,
+    v4float64, ML_String, ML_Binary64, VECTOR_TYPE_MAP)
 from metalibm_core.core.display_utils import DisplayFormat
 from metalibm_core.core.target import TargetRegister
 from metalibm_core.core.ml_operations import BuildFromComponent, ComponentSelection
@@ -56,14 +57,42 @@ from metalibm_core.code_generation.complex_generator import DynamicOperator
 DISPLAY_SLEEF_VDOUBLE_2 = DisplayFormat(format_string="{{.x=[%a, %a, %a, %a], .y=[%a, %a, %a, %a]}}", pre_process_fct= lambda v: ", ".join(["%s.x[%d]" % (v, i) for i in range(4)] + ["%s.y[%d]" % (v, i) for i in range(4)]))
 DISPLAY_SLEEF_DOUBLE_2  = DisplayFormat(format_string="{{.x=%a, .y=%a}}", pre_process_fct= lambda v: "%s.x, %s.y " % (v, v))
 
-# defining a custom types
-Sleef_SLEEF_VECTOR_DOUBLE_2 = ML_Compound_Format("Sleef_SLEEF_VECTOR_DOUBLE_2",
-                                                 ["x", "y"],
-                                                 [v4float64, v4float64],
-                                                 None,
-                                                 DISPLAY_SLEEF_VDOUBLE_2,
-                                                 sollya.error,
-                                                 header=["sleef.h"])
+class SleefCompoundVectorFormat(ML_CompoundVectorFormat):
+    """ specific Compound(struct) format class for Sleef vector compounded
+        vector types """
+    def __init__(self, c_format_name, vector_size,
+                   field_list, field_format_list,
+                   scalar_format, sollya_precision=None,
+                   cst_callback=None,
+                   display_format="",
+                   header=None):
+        ML_VectorFormat.__init__(self, scalar_format, vector_size, c_format_name, header=header)
+        # header must be re-submitted as argument to avoid being
+        # over written by this new constructor call
+        ML_Compound_Format.__init__(self, c_format_name, field_list, field_format_list, "", display_format, sollya_precision, header=header)
+        self.cst_callback = cst_callback
+        self.limb_num = 2
+
+    def get_limb_precision(self, index):
+        return self.field_format_list[index]
+
+    def get_cst_default(self, cst_value, language = C_Code):
+        elt_value_list = [self.scalar_format.get_cst(cst_value[i], language = language) for i in range(self.vector_size)]
+        field_str_list = [".%s" for field_name in self.scalar_format.c_field_list]
+        cst_value_array = [[None for i in range(self.vector_size)] for j in range(self.limb_num)]
+        for lane_id in range(self.vector_size):
+            tmp_cst = cst_value[lane_id]
+            if tmp_cst != 0:
+                Log.report(Log.Error, "only zero cst_value is supported in SleefCompoundVectorFormat.get_cst_default, not {}", tmp_cst)
+            assert tmp_cst == 0
+            for limb_id in range(self.limb_num):
+                field_format = self.field_format_list[limb_id]
+                field_value = sollya.round(tmp_cst, field_format.sollya_object, sollya.RN)
+                cst_value_array[limb_id][lane_id] = field_value
+        if language is C_Code:
+            return "{" + ",".join("{} = {}".format(field_str_list[limb_id], self.get_limb_precision(limb_id).get_cst(cst_value_array[limb_id], language=language)) for limb_id in range(self.limb_num)) + "}"
+        else:
+            Log.report(Log.Error, "unsupported language in ML_MultiPrecision_VectorFormat.get_cst: %s" % (language))
 
 Sleef_SLEEF_DOUBLE_2 = ML_Compound_Format("Sleef_SLEEF_DOUBLE_2",
                   ["x", "y"],
@@ -72,6 +101,21 @@ Sleef_SLEEF_DOUBLE_2 = ML_Compound_Format("Sleef_SLEEF_DOUBLE_2",
                   DISPLAY_SLEEF_DOUBLE_2,
 				  sollya.error,
 				  header=["sleef.h"])
+
+# defining custom types
+Sleef_SLEEF_VECTOR_DOUBLE_2 = SleefCompoundVectorFormat("Sleef_SLEEF_VECTOR_DOUBLE_2",
+                                                 4,
+                                                 ["x", "y"],
+                                                 [v4float64, v4float64],
+                                                 Sleef_SLEEF_DOUBLE_2,
+                                                 sollya_precision=sollya.error,
+                                                 cst_callback=None,
+                                                 display_format=DISPLAY_SLEEF_VDOUBLE_2,
+                                                 header=["sleef.h"])
+
+
+# registering sleef type for vectorisation
+VECTOR_TYPE_MAP[Sleef_SLEEF_DOUBLE_2] = {4: Sleef_SLEEF_VECTOR_DOUBLE_2}
 
 # registering it as a parsable type
 template_module.precision_map["sleef_svd2"] = Sleef_SLEEF_VECTOR_DOUBLE_2
