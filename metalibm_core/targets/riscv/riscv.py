271fa0b905ba5e2003879bdbc7ff999c1dabc1f6
Nicolas Brunie <nibrunie@gmail.com>
2021-09-06 21:50:59 +0200

    [RISCV] adding basic build and simulation capabilities
    * Connecting basic cross-compilation toolchain through envvar
    * Connecting spike isa-sim as executor
    * cycle measurement op mapping

--- metalibm_core/targets/riscv/riscv.py
+++ metalibm_core/targets/riscv/riscv.py
@@ -31,27 +31,73 @@
 # Author(s):        Nicolas Brunie <metalibmdev@gmail.com>
 ###############################################################################
 
+import os
+
 from metalibm_core.core.target import UniqueTargetDecorator
+from metalibm_core.core.ml_operations import SpecificOperation
+from metalibm_core.core.ml_formats import ML_Int64
 
 from metalibm_core.code_generation.abstract_backend import LOG_BACKEND_INIT
 from metalibm_core.code_generation.code_constant import C_Code
+from metalibm_core.code_generation.generator_utility import (
+    AsmInlineOperator, FO_Result, type_strict_match)
 
 from metalibm_core.utility.log_report import Log
 
 from metalibm_core.targets.common.vector_backend import VectorBackend
 
 
+rdcycleOperator = AsmInlineOperator(
+"""{
+    unsigned long cycles;
+    asm volatile ("rdcycle %%0 " : "=r" (cycles));
+    %s = cycles;
+}""",
+    arg_map = {0: FO_Result(0)},
+    arity = 0
+)
+
+rv64CCodeGenTable = {
+    SpecificOperation: {
+        SpecificOperation.ReadTimeStamp: {
+            lambda _: True: {
+                type_strict_match(ML_Int64): rdcycleOperator
+            }
+        }
+    },
+}
+
 
 @UniqueTargetDecorator
 class RISCV_RV64(VectorBackend):
     target_name = "rv64g"
+    default_compiler = "{}/bin/riscv64-unknown-elf-gcc".format(os.environ["RISCV"])
+    # only cross-compilation (not binary embedding in python) is currently supported
+    support_embedded_bin = False
+    cross_platform = True
 
     code_generation_table = {
-        C_Code: {},
+        C_Code: rv64CCodeGenTable,
     }
 
     def __init__(self):
         super().__init__()
+
+    def get_compilation_options(self, ML_SRC_DIR):
+        return super(RISCV_RV64, self).get_compilation_options(ML_SRC_DIR) + ["-march=rv64gc"]
+
+    def get_execution_command(self, test_file):
+        try:
+            pk_bin = os.environ["PK_BIN"]
+        except:
+            Log.report(Log.Error, "PK_BIN env var must point to proxy-kernel image")
+
+        try:
+            spike_bin = os.environ["SPIKE_BIN"]
+        except:
+            Log.report(Log.Error, "SPIKE_BIN env var must point to spike simulator binary")
+        cmd = "{} --isa=RV64gc {}  {}".format(spike_bin, pk_bin, test_file)
+        return cmd
 
 # debug message
 Log.report(LOG_BACKEND_INIT, "initializing RISC-V targets")
