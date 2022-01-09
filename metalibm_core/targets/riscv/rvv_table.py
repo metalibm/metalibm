fb5eb9f67a560adeac23050e34fcbef5feb968c6
Nicolas Brunie <nibrunie@gmail.com>
2022-01-09 15:47:32 +0100

    Adding support for 64-bit ReciprocalSeed in RVV backend

--- metalibm_core/targets/riscv/rvv_table.py
+++ metalibm_core/targets/riscv/rvv_table.py
@@ -39,30 +39,33 @@ from metalibm_core.core.ml_operations import ReciprocalSeed
 import sollya
 
 from metalibm_core.core.ml_table import ML_ApproxTable
-from metalibm_core.core.ml_formats import ML_Binary32
+from metalibm_core.core.ml_formats import ML_Binary32, ML_Binary64
 
 
-rvv_vfrec7_table = ML_ApproxTable(
-    dimensions = [2**7], 
-    index_size=7,
-    storage_precision = ML_Binary32,
-    init_data = [ 1.0 + v / sollya.SollyaObject(128) for v in 
-            [127, 125, 123, 121, 119, 117, 116, 114,
-             112, 110, 109, 107, 105, 104, 102, 100,
-             99, 97, 96, 94, 93, 91, 90, 88, 87, 85,
-             84, 83, 81, 80, 79, 77, 76, 75, 74, 72,
-             71, 70, 69, 68, 66, 65, 64, 63, 62, 61,
-             60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
-             50, 49, 48, 47, 46, 45, 44, 43, 42, 41,
-             40, 40, 39, 38, 37, 36, 35, 35, 34, 33,
-             32, 31, 31, 30, 29, 28, 28, 27, 26, 25,
-             25, 24, 23, 23, 22, 21, 21, 20, 19, 19,
-             18, 17, 17, 16, 15, 15, 14, 14, 13, 12,
-             12, 11, 11, 10, 9, 9, 8, 8, 7, 7, 6, 5,
-             5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 
-            ]
-    ]
-)
+def rvv_vfrec7_table_ctor(storageType):
+    assert storageType.get_field_size() >= 7, "storageType precision must exceeds table accuracy"
+    return ML_ApproxTable(
+        dimensions = [2**7], 
+        index_size=7,
+        storage_precision = storageType,
+        # the following constants should be exact in storageType
+        init_data = [ (1.0 + v / sollya.SollyaObject(128)) / 2 for v in 
+                [127, 125, 123, 121, 119, 117, 116, 114,
+                112, 110, 109, 107, 105, 104, 102, 100,
+                99, 97, 96, 94, 93, 91, 90, 88, 87, 85,
+                84, 83, 81, 80, 79, 77, 76, 75, 74, 72,
+                71, 70, 69, 68, 66, 65, 64, 63, 62, 61,
+                60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
+                50, 49, 48, 47, 46, 45, 44, 43, 42, 41,
+                40, 40, 39, 38, 37, 36, 35, 35, 34, 33,
+                32, 31, 31, 30, 29, 28, 28, 27, 26, 25,
+                25, 24, 23, 23, 22, 21, 21, 20, 19, 19,
+                18, 17, 17, 16, 15, 15, 14, 14, 13, 12,
+                12, 11, 11, 10, 9, 9, 8, 8, 7, 7, 6, 5,
+                5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 
+                ]
+        ]
+    )
 
 
 rvv_approx_table_map = {
@@ -70,7 +73,8 @@ rvv_approx_table_map = {
         ReciprocalSeed: {
             None: {
                 lambda optree: True: {
-                    type_strict_match(ML_Binary32, ML_Binary32): rvv_vfrec7_table,
+                    type_strict_match(ML_Binary32, ML_Binary32): rvv_vfrec7_table_ctor(ML_Binary32),
+                    type_strict_match(ML_Binary64, ML_Binary64): rvv_vfrec7_table_ctor(ML_Binary64),
                 },
             },
         },
